694d5fb3484b99510b1a653344a9067bbddde24c
Montel Laurent <montel@kde.org>
2015-12-05 00:04:23 +0100

    Add antivirus plugin

---
+++ kmail/plugins/antivirusplugin/antivirusplugininterface.cpp
@@ -0,0 +1,55 @@
+/*
+  Copyright (c) 2015 Montel Laurent <montel@kde.org>
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+#include "antivirusplugininterface.h"
+#include "antispamwizard.h"
+#include <KLocalizedString>
+#include <KToggleAction>
+#include <KActionCollection>
+#include <QAction>
+
+AntiVirusPluginInterface::AntiVirusPluginInterface(QObject *parent)
+    : PimCommon::GenericPluginInterface(parent)
+{
+
+}
+
+AntiVirusPluginInterface::~AntiVirusPluginInterface()
+{
+
+}
+
+void AntiVirusPluginInterface::createAction(KActionCollection *ac)
+{
+    KToggleAction *action = new KToggleAction(i18n("&Anti-Virus Wizard..."), this);
+    action->setCheckable(false);
+    ac->addAction(QStringLiteral("antiVirusWizard"), action);
+    connect(action, &QAction::triggered, this, &AntiVirusPluginInterface::slotAntiVirusWizard);
+    //TODO actionType
+}
+
+void AntiVirusPluginInterface::exec()
+{
+    slotAntiVirusWizard();
+}
+
+void AntiVirusPluginInterface::slotAntiVirusWizard()
+{
+    KMail::AntiSpamWizard wiz(KMail::AntiSpamWizard::AntiVirus, parentWidget());
+    wiz.exec();
+}
---
+++ kmail/plugins/antivirusplugin/antivirusplugininterface.h
@@ -0,0 +1,37 @@
+/*
+  Copyright (c) 2015 Montel Laurent <montel@kde.org>
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+#ifndef ANTIVIRUSPLUGININTERFACE_H
+#define ANTIVIRUSPLUGININTERFACE_H
+
+#include <pimcommon/genericplugininterface.h>
+
+class AntiVirusPluginInterface : public PimCommon::GenericPluginInterface
+{
+public:
+    explicit AntiVirusPluginInterface(QObject *parent = Q_NULLPTR);
+    ~AntiVirusPluginInterface();
+
+    void exec() Q_DECL_OVERRIDE;
+    void createAction(KActionCollection *ac) Q_DECL_OVERRIDE;
+
+private Q_SLOTS:
+    void slotAntiVirusWizard();
+};
+
+#endif // ANTIVIRUSPLUGININTERFACE_H
