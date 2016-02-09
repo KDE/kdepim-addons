0321e5049d3a6eff381d9733ecde9ae394021aad
Sandro Knauß <sknauss@kde.org>
2016-02-09 14:19:42 +0100

    Move tnef render test to plugins, to now have a circular dependency.
    Differential Revision: https://phabricator.kde.org/D923

---
+++ plugins/messageviewer/bodypartformatter/autotests/rendertest.cpp
@@ -0,0 +1,123 @@
+/*
+  Copyright (c) 2010 Volker Krause <vkrause@kde.org>
+  Copyright (c) 2016 Sandro Knauß <sknauss@kde.org>
+
+  This library is free software; you can redistribute it and/or modify it
+  under the terms of the GNU Library General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or (at your
+  option) any later version.
+
+  This library is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+  License for more details.
+
+  You should have received a copy of the GNU Library General Public License
+  along with this library; see the file COPYING.LIB.  If not, write to the
+  Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+  02110-1301, USA.
+*/
+
+#include "rendertest.h"
+
+#include "testobjecttreesource.h"
+
+#include <MessageViewer/ObjectTreeParser>
+#include <MessageViewer/CSSHelperBase>
+#include <MessageViewer/FileHtmlWriter>
+
+#include <KMime/Message>
+#include <QTest>
+#include <QDir>
+#include <QProcess>
+
+void RenderTest::testRender_data()
+{
+    QTest::addColumn<QString>("mailFileName");
+    QTest::addColumn<QString>("referenceFileName");
+    QTest::addColumn<QString>("outFileName");
+
+    QDir dir(QStringLiteral(MAIL_DATA_DIR));
+    foreach (const QString &file, dir.entryList(QStringList(QLatin1String("*.mbox")), QDir::Files | QDir::Readable | QDir::NoSymLinks)) {
+        if (!QFile::exists(dir.path() + QLatin1Char('/') + file + QLatin1String(".html"))) {
+            continue;
+        }
+        QTest::newRow(file.toLatin1()) << QString(dir.path() + QLatin1Char('/') +  file) << QString(dir.path() + QLatin1Char('/') + file + QLatin1String(".html")) << QString(file + QLatin1String(".out"));
+    }
+}
+
+void RenderTest::testRender()
+{
+    QFETCH(QString, mailFileName);
+    QFETCH(QString, referenceFileName);
+    QFETCH(QString, outFileName);
+
+    const QString htmlFileName = outFileName + QLatin1String(".html");
+
+    // load input mail
+    QFile mailFile(mailFileName);
+    QVERIFY(mailFile.open(QIODevice::ReadOnly));
+    const QByteArray mailData = KMime::CRLFtoLF(mailFile.readAll());
+    QVERIFY(!mailData.isEmpty());
+    KMime::Message::Ptr msg(new KMime::Message);
+    msg->setContent(mailData);
+    msg->parse();
+
+    // render the mail
+    MessageViewer::FileHtmlWriter fileWriter(outFileName);
+    QImage paintDevice;
+    MessageViewer::CSSHelperBase cssHelper(&paintDevice);
+    MessageViewer::NodeHelper nodeHelper;
+    TestObjectTreeSource testSource(&fileWriter, &cssHelper);
+    MessageViewer::ObjectTreeParser otp(&testSource, &nodeHelper);
+
+    fileWriter.begin(QString());
+    fileWriter.queue(QStringLiteral("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">\n"
+                      "<html>\n"
+                      "<body>\n"));
+
+    otp.parseObjectTree(msg.data());
+
+    fileWriter.queue(QStringLiteral("</body></html>"));
+    fileWriter.flush();
+    fileWriter.end();
+
+    QVERIFY(QFile::exists(outFileName));
+
+    // validate xml and pretty-print for comparisson
+    // TODO add proper cmake check for xmllint and diff
+    QStringList args = QStringList()
+                        << QStringLiteral("--format")
+                        << QStringLiteral("--encode")
+                        << QStringLiteral("UTF8")
+                        << QStringLiteral("--output")
+                        << htmlFileName
+                        << outFileName;
+    QCOMPARE(QProcess::execute(QLatin1String("xmllint"), args),  0);
+
+    // get rid of system dependent or random paths
+    {
+        QFile f(htmlFileName);
+        QVERIFY(f.open(QIODevice::ReadOnly));
+        QString content = QString::fromUtf8(f.readAll());
+        f.close();
+        content.replace(QRegExp(QLatin1String("\"file:[^\"]*[/(?:%2F)]([^\"/(?:%2F)]*)\"")), QStringLiteral("\"file:\\1\""));
+        QVERIFY(f.open(QIODevice::WriteOnly | QIODevice::Truncate));
+        f.write(content.toUtf8());
+        f.close();
+    }
+
+    // compare to reference file
+    args = QStringList()
+            << QStringLiteral("-u")
+            << referenceFileName
+            << htmlFileName;
+    QProcess proc;
+    proc.setProcessChannelMode(QProcess::ForwardedChannels);
+    proc.start(QStringLiteral("diff"), args);
+    QVERIFY(proc.waitForFinished());
+
+    QCOMPARE(proc.exitCode(), 0);
+}
+
+QTEST_MAIN(RenderTest)
---
+++ plugins/messageviewer/bodypartformatter/autotests/testobjecttreesource.h
@@ -0,0 +1,124 @@
+/*
+  Copyright (C) 2010 Klaralvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
+  Copyright (c) 2010 Leo Franchi <lfranchi@kde.org>
+
+  This library is free software; you can redistribute it and/or modify it
+  under the terms of the GNU Library General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or (at your
+  option) any later version.
+
+  This library is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+  License for more details.
+
+  You should have received a copy of the GNU Library General Public License
+  along with this library; see the file COPYING.LIB.  If not, write to the
+  Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+  02110-1301, USA.
+*/
+
+#ifndef MESSAGECORE_TESTS_UTIL_H
+#define MESSAGECORE_TESTS_UTIL_H
+
+#include <MessageViewer/ObjectTreeSourceIf>
+#include <MessageViewer/CSSHelperBase>
+#include <MessageViewer/AttachmentStrategy>
+
+class TestObjectTreeSource : public MessageViewer::ObjectTreeSourceIf
+{
+public:
+    TestObjectTreeSource(MessageViewer::HtmlWriter *writer,
+                         MessageViewer::CSSHelperBase *cssHelper)
+        : mWriter(writer)
+        , mCSSHelper(cssHelper)
+        , mHtmlLoadExternal(false)
+        , mHtmlMail(true)
+    {
+    }
+
+    MessageViewer::HtmlWriter *htmlWriter() Q_DECL_OVERRIDE
+    {
+        return mWriter;
+    }
+
+    MessageViewer::CSSHelperBase *cssHelper() Q_DECL_OVERRIDE
+    {
+        return mCSSHelper;
+    }
+
+    bool htmlLoadExternal() const Q_DECL_OVERRIDE
+    {
+        return mHtmlLoadExternal;
+    }
+
+    void setHtmlLoadExternal(bool loadExternal)
+    {
+        mHtmlLoadExternal = loadExternal;
+    }
+
+    bool htmlMail() const Q_DECL_OVERRIDE
+    {
+        return mHtmlMail;
+    }
+
+    void setHtmlMail(bool htmlMail)
+    {
+        mHtmlMail = htmlMail;
+    }
+
+    bool decryptMessage() const Q_DECL_OVERRIDE
+    {
+        return false;
+    }
+    bool showSignatureDetails() const Q_DECL_OVERRIDE
+    {
+        return false;
+    }
+    void setHtmlMode(MessageViewer::Util::HtmlMode mode) Q_DECL_OVERRIDE
+    {
+        Q_UNUSED(mode)
+    }
+    int levelQuote() const Q_DECL_OVERRIDE
+    {
+        return false;
+    }
+    const QTextCodec *overrideCodec() Q_DECL_OVERRIDE
+    {
+        return 0;
+    }
+    QString createMessageHeader(KMime::Message *message) Q_DECL_OVERRIDE
+    {
+        Q_UNUSED(message);
+        return QString();
+    }
+    const MessageViewer::AttachmentStrategy *attachmentStrategy() Q_DECL_OVERRIDE
+    {
+        return MessageViewer::AttachmentStrategy::smart();
+    }
+    QObject *sourceObject() Q_DECL_OVERRIDE
+    {
+        return 0;
+    }
+    
+    bool autoImportKeys() const Q_DECL_OVERRIDE
+    {
+        return false;
+    }
+    bool showEmoticons() const Q_DECL_OVERRIDE
+    {
+        return false;
+    }
+    bool showExpandQuotesMark() const Q_DECL_OVERRIDE
+    {
+        return false;
+    }
+    
+private:
+    MessageViewer::HtmlWriter *mWriter;
+    MessageViewer::CSSHelperBase *mCSSHelper;
+    bool mHtmlLoadExternal;
+    bool mHtmlMail;
+};
+
+#endif
