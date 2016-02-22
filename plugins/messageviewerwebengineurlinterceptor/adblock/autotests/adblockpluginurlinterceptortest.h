fe4636697d855a3013eb5d3abbdc806b3fecb3b9
Montel Laurent <montel@kde.org>
2016-02-22 07:41:17 +0100

    Add more autotest

---
+++ plugins/messageviewerwebengineurlinterceptor/adblock/autotests/adblockpluginurlinterceptortest.h
@@ -0,0 +1,14 @@
+#ifndef ADBLOCKPLUGINURLINTERCEPTORTEST_H
+#define ADBLOCKPLUGINURLINTERCEPTORTEST_H
+
+#include <QObject>
+
+class AdblockPluginUrlInterceptorTest : public QObject
+{
+    Q_OBJECT
+public:
+    explicit AdblockPluginUrlInterceptorTest(QObject *parent = Q_NULLPTR);
+    ~AdblockPluginUrlInterceptorTest();
+};
+
+#endif // ADBLOCKPLUGINURLINTERCEPTORTEST_H
