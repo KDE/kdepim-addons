71e60676c1acc5fec49fea2bb3ea516015c1bf96
Montel Laurent <montel@kde.org>
2016-03-28 21:25:00 +0200

    Add external script manager

---
+++ plugins/messageviewerplugins/externalscriptplugin/viewerpluginexternalscriptmanager.cpp
@@ -0,0 +1,53 @@
+/*
+  Copyright (c) 2016 Montel Laurent <montel@kde.org>
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+#include "viewerpluginexternalscriptmanager.h"
+
+class ViewerPluginExternalScriptManagerInstancePrivate
+{
+public:
+    ViewerPluginExternalScriptManagerInstancePrivate()
+        : externalScriptPluginManager(new ViewerPluginExternalScriptManager)
+    {
+    }
+
+    ~ViewerPluginExternalScriptManagerInstancePrivate()
+    {
+        delete externalScriptPluginManager;
+    }
+
+    ViewerPluginExternalScriptManager *externalScriptPluginManager;
+};
+
+Q_GLOBAL_STATIC(ViewerPluginExternalScriptManagerInstancePrivate, sInstance)
+
+ViewerPluginExternalScriptManager::ViewerPluginExternalScriptManager(QObject *parent)
+    : QObject(parent)
+{
+
+}
+
+ViewerPluginExternalScriptManager::~ViewerPluginExternalScriptManager()
+{
+
+}
+
+ViewerPluginExternalScriptManager *ViewerPluginExternalScriptManager::self()
+{
+    return sInstance->externalScriptPluginManager;
+}
