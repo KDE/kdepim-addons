b54a8cc9bef647b33c354b8422d8bb13d44d0783
Montel Laurent <montel@kde.org>
2016-06-02 13:33:52 +0200

    Add configurewidget

---
+++ kmail/editorsendcheckplugins/confirm-address/autotests/confirmaddressconfigurewidgettest.cpp
@@ -0,0 +1,46 @@
+/*
+   Copyright (C) 2016 Montel Laurent <montel@kde.org>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; see the file COPYING.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+
+#include "confirmaddressconfigurewidgettest.h"
+#include "../confirmaddressconfigurewidget.h"
+
+#include <QTest>
+#include <QVBoxLayout>
+
+ConfirmAddressConfigureWidgetTest::ConfirmAddressConfigureWidgetTest(QObject *parent)
+    : QObject(parent)
+{
+
+}
+
+ConfirmAddressConfigureWidgetTest::~ConfirmAddressConfigureWidgetTest()
+{
+
+}
+
+void ConfirmAddressConfigureWidgetTest::shouldHaveDefaultValue()
+{
+    ConfirmAddressConfigureWidget w;
+
+    QVBoxLayout *vboxlayout = w.findChild<QVBoxLayout *>(QStringLiteral("mainlayout"));
+    QVERIFY(vboxlayout);
+}
+
+QTEST_MAIN(ConfirmAddressConfigureWidgetTest)
