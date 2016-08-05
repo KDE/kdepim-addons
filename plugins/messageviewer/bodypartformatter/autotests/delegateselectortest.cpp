09f84fc16956d637d52786432e68a077d9a39ec0
Montel Laurent <montel@kde.org>
2016-08-05 13:43:29 +0200

    Fix layout of delegateselector. I think that nobody uses this feature.
    Add autotests too to be sure that it will not break in the future

---
+++ plugins/messageviewer/bodypartformatter/autotests/delegateselectortest.cpp
@@ -0,0 +1,58 @@
+/*
+   Copyright (C) 2016 Montel Laurent <montel@kde.org>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; see the file COPYING.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "delegateselectortest.h"
+#include "../calendar/delegateselector.h"
+#include <QTest>
+#include <QLabel>
+#include <QCheckBox>
+#include <QDialogButtonBox>
+#include <Libkdepim/AddresseeLineEdit>
+
+DelegateSelectorTest::DelegateSelectorTest(QObject *parent)
+    : QObject(parent)
+{
+
+}
+
+DelegateSelectorTest::~DelegateSelectorTest()
+{
+
+}
+
+void DelegateSelectorTest::shouldHaveDefaultValue()
+{
+    DelegateSelector w;
+    QLabel *label = w.findChild<QLabel *>(QStringLiteral("label"));
+    QVERIFY(label);
+    QVERIFY(!label->text().isEmpty());
+
+    KPIM::AddresseeLineEdit *mDelegate = w.findChild<KPIM::AddresseeLineEdit *>(QStringLiteral("delegate"));
+    QVERIFY(mDelegate);
+
+    QCheckBox *mRsvp = w.findChild<QCheckBox *>(QStringLiteral("informcheckbox"));
+    QVERIFY(mRsvp);
+    QVERIFY(mRsvp->isChecked());
+    QVERIFY(!mRsvp->text().isEmpty());
+
+    QDialogButtonBox *buttonBox = w.findChild<QDialogButtonBox *>(QStringLiteral("buttonbox"));
+    QVERIFY(buttonBox);
+}
+
+QTEST_MAIN(DelegateSelectorTest)
