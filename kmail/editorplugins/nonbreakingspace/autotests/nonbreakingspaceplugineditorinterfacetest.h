75191ad8695117bb586919ead4e3e5dea28ddf44
Montel Laurent <montel@kde.org>
2016-08-08 08:00:28 +0200

    Add insert breaking space as plugin

---
+++ kmail/editorplugins/nonbreakingspace/autotests/nonbreakingspaceplugineditorinterfacetest.h
@@ -0,0 +1,35 @@
+/*
+   Copyright (C) 2016 Montel Laurent <montel@kde.org>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; see the file COPYING.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#ifndef NonBreakingSpacePLUGINEDITORINTERFACETEST_H
+#define NonBreakingSpacePLUGINEDITORINTERFACETEST_H
+
+#include <QObject>
+
+class NonBreakingSpacePluginEditorInterfaceTest : public QObject
+{
+    Q_OBJECT
+public:
+    explicit NonBreakingSpacePluginEditorInterfaceTest(QObject *parent = Q_NULLPTR);
+    ~NonBreakingSpacePluginEditorInterfaceTest();
+private Q_SLOTS:
+    void shouldHaveDefaultValue();
+};
+
+#endif
---
+++ kmail/editorplugins/nonbreakingspace/nonbreakingspaceplugineditorinterface.cpp
@@ -0,0 +1,54 @@
+/*
+   Copyright (C) 2016 Montel Laurent <montel@kde.org>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; see the file COPYING.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "nonbreakingspaceplugineditorinterface.h"
+#include <KPIMTextEdit/RichTextEditor>
+#include <KLocalizedString>
+#include <KActionCollection>
+#include <QAction>
+#include <QDebug>
+NonBreakingSpacePluginEditorInterface::NonBreakingSpacePluginEditorInterface(QObject *parent)
+    : MessageComposer::PluginEditorInterface(parent)
+{
+}
+
+NonBreakingSpacePluginEditorInterface::~NonBreakingSpacePluginEditorInterface()
+{
+
+}
+
+void NonBreakingSpacePluginEditorInterface::createAction(KActionCollection *ac)
+{
+    QAction *action = new QAction(i18n("Insert Non Breaking Space"), this);
+    ac->addAction(QStringLiteral("insert_non_breaking_space"), action);
+    ac->setDefaultShortcut(action, Qt::CTRL + Qt::Key_Space);
+    connect(action, &QAction::triggered, this, &NonBreakingSpacePluginEditorInterface::slotActivated);
+    MessageComposer::ActionType type(action, MessageComposer::ActionType::Edit);
+    setActionType(type);
+}
+
+void NonBreakingSpacePluginEditorInterface::slotActivated()
+{
+    Q_EMIT emitPluginActivated(this);
+}
+
+void NonBreakingSpacePluginEditorInterface::exec()
+{
+    richTextEditor()->insertPlainText(QChar(0x000A0));
+}
