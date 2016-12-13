a48943c6facf60d1d0abb774fb1e4b66bf3ea2bc
Montel Laurent <montel@kde.org>
2016-12-13 13:31:43 +0100

    Add autotest

---
+++ sieveeditor/imapfoldercompletion/autotests/selectimapwidgettest.cpp
@@ -0,0 +1,51 @@
+/*
+   Copyright (C) 2016 Montel Laurent <montel@kde.org>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; see the file COPYING.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "selectimapwidgettest.h"
+#include "../selectimapwidget.h"
+#include <KLineEdit>
+
+#include <QTest>
+#include <QHBoxLayout>
+#include <QToolButton>
+
+SelectImapWidgetTest::SelectImapWidgetTest(QObject *parent)
+    : QObject(parent)
+{
+
+}
+
+SelectImapWidgetTest::~SelectImapWidgetTest()
+{
+
+}
+
+void SelectImapWidgetTest::shouldHaveDefaultValue()
+{
+    SelectImapWidget w;
+    KLineEdit *mLineEdit = w.findChild<KLineEdit *>(QStringLiteral("lineedit"));
+    QVERIFY(mLineEdit);
+    QVERIFY(mLineEdit->text().isEmpty());
+
+    QToolButton *mToolButton = w.findChild<QToolButton *>(QStringLiteral("toolbutton"));
+    QVERIFY(mToolButton);
+    QVERIFY(!mToolButton->text().isEmpty());
+}
+
+QTEST_MAIN(SelectImapWidgetTest)
