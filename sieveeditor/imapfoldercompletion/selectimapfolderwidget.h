437b26e60e18df2bb078eeba1db94e9068d932e9
Montel Laurent <montel@kde.org>
2016-12-13 13:19:34 +0100

    Add more code for plugins

---
+++ sieveeditor/imapfoldercompletion/selectimapfolderwidget.h
@@ -0,0 +1,40 @@
+/*
+   Copyright (C) 2016 Montel Laurent <montel@kde.org>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; see the file COPYING.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#ifndef SELECTIMAPFOLDERWIDGET_H
+#define SELECTIMAPFOLDERWIDGET_H
+
+#include <QWidget>
+namespace KIMAP {
+class Session;
+}
+class QTreeView;
+class SelectImapFolderWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit SelectImapFolderWidget(QWidget *parent = Q_NULLPTR);
+    ~SelectImapFolderWidget();
+private:
+    void loadImapFolders();
+    QTreeView *mTreeView;
+    KIMAP::Session *m_session;
+};
+
+#endif // SELECTIMAPFOLDERWIDGET_H
