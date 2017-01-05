bed72b8fa28b9680d1cd880bf6b064aacf35a2e3
Montel Laurent <montel@kde.org>
2017-01-05 20:53:05 +0100

    GIT_SILENT: fix copyright

--- kaddressbook/plugins/sendmail/mailsenderjob.h
+++ kaddressbook/plugins/sendmail/mailsenderjob.h
@@ -1,7 +1,7 @@
 /*
   This file is part of KAddressBook.
 
-  Copyright (c) 2014-2016 Laurent Montel <montel@kde.org>
+  Copyright (C) 2014-2017 Laurent Montel <montel@kde.org>
   based on code from Copyright (c) 2014 Clément Vannier <clement.vannier@free.fr>
 
   This program is free software; you can redistribute it and/or modify
--- plugins/messageviewerheaderplugins/briefheaderstyleplugin/briefheaderstyle.h
+++ plugins/messageviewerheaderplugins/briefheaderstyleplugin/briefheaderstyle.h
@@ -4,7 +4,7 @@
     This file is part of KMail, the KDE mail client.
     Copyright (c) 2003 Marc Mutz <mutz@kde.org>
 
-    Copyright (C) 2013-2016 Laurent Montel <montel@kde.org>
+    Copyright (C) 2013-2017 Laurent Montel <montel@kde.org>
 
     KMail is free software; you can redistribute it and/or modify it
     under the terms of the GNU General Public License, version 2, as
