69da21851f1bda8777c001137df20d1e092b63d8
Montel Laurent <montel@kde.org>
2017-06-13 08:10:10 +0200

    Add autotests

---
+++ plugins/templateparser/autotests/templateparseremailaddressrequesterakonaditest.h
@@ -0,0 +1,15 @@
+#ifndef TEMPLATEPARSEREMAILADDRESSREQUESTERAKONADITEST_H
+#define TEMPLATEPARSEREMAILADDRESSREQUESTERAKONADITEST_H
+
+#include <QObject>
+
+class TemplateParserEmailAddressRequesterAkonadiTest : public QObject
+{
+    Q_OBJECT
+public:
+    explicit TemplateParserEmailAddressRequesterAkonadiTest(QObject *parent = nullptr);
+     ~TemplateParserEmailAddressRequesterAkonadiTest() = default;
+
+};
+
+#endif // TEMPLATEPARSEREMAILADDRESSREQUESTERAKONADITEST_H
