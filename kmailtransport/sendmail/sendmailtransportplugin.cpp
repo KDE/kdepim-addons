463a9ca8761bdf3384eb486aad79ded7a5ee8626
Montel Laurent <montel@kde.org>
2017-06-27 08:12:41 +0200

    Start to create sendmail transport

---
+++ kmailtransport/sendmail/sendmailtransportplugin.cpp
@@ -0,0 +1,55 @@
+/*
+    Copyright (c) 2017 Laurent Montel <montel@kde.org>
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+    License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to the
+    Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301, USA.
+*/
+
+#include "sendmailtransportplugin.h"
+#include <kpluginfactory.h>
+
+K_PLUGIN_FACTORY_WITH_JSON(SendMailTransportPluginFactory, "sendmailtransport.json", registerPlugin<SendMailTransportPlugin>();
+                           )
+
+SendMailTransportPlugin::SendMailTransportPlugin(QObject *parent, const QList<QVariant> &)
+    : MailTransport::TransportAbstractPlugin(parent)
+{
+
+}
+
+SendMailTransportPlugin::~SendMailTransportPlugin()
+{
+
+}
+
+QString SendMailTransportPlugin::name() const
+{
+    //TODO FIXME
+    return {};
+}
+
+bool SendMailTransportPlugin::configureTransport(MailTransport::Transport *transport, QWidget *parent)
+{
+    //TODO FIXME
+    return false;
+}
+
+void SendMailTransportPlugin::cleanUp(const QString &identifier)
+{
+    Q_UNUSED(identifier);
+    //TODO FIXME
+}
+
+#include "sendmailtransportplugin.moc"
---
+++ kmailtransport/sendmail/sendmailtransportplugin.h
@@ -0,0 +1,38 @@
+/*
+    Copyright (c) 2017 Laurent Montel <montel@kde.org>
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+    License for more details.
+
+    You should have received a copy of the GNU Library General Public License
+    along with this library; see the file COPYING.LIB.  If not, write to the
+    Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301, USA.
+*/
+
+#ifndef SendMAILTRANSPORTPLUGIN_H
+#define SendMAILTRANSPORTPLUGIN_H
+
+#include <MailTransport/TransportAbstractPlugin>
+#include <QVariant>
+
+class SendMailTransportPlugin : public MailTransport::TransportAbstractPlugin
+{
+    Q_OBJECT
+public:
+    explicit SendMailTransportPlugin(QObject *parent = nullptr, const QList<QVariant> & = {});
+    ~SendMailTransportPlugin();
+
+    QString name() const override;
+    bool configureTransport(MailTransport::Transport *transport, QWidget *parent) override;
+    void cleanUp(const QString &identifier) override;
+};
+
+#endif // SendMAILTRANSPORTPLUGIN_H
