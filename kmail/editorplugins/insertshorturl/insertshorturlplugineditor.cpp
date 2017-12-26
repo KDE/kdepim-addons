13a33b770a2730fd926bad9b419bb95abaf2415a
Laurent Montel <montel@kde.org>
2017-12-26 13:13:01 +0100

    Start to create shorter url plugin

---
+++ kmail/editorplugins/insertshorturl/insertshorturlplugineditor.cpp
@@ -0,0 +1,43 @@
+/*
+   Copyright (C) 2017 Montel Laurent <montel@kde.org>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; see the file COPYING.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "insertshorturlplugineditor.h"
+#include "insertshorturlplugineditorinterface.h"
+#include <kpluginfactory.h>
+
+K_PLUGIN_FACTORY_WITH_JSON(InsertShorturlPluginEditorFactory, "kmail_insertshorturleditorplugin.json", registerPlugin<InsertShorturlPluginEditor>();
+                           )
+
+InsertShorturlPluginEditor::InsertShorturlPluginEditor(QObject *parent, const QList<QVariant> &)
+    : MessageComposer::PluginEditor(parent)
+{
+}
+
+InsertShorturlPluginEditor::~InsertShorturlPluginEditor()
+{
+}
+
+MessageComposer::PluginEditorInterface *InsertShorturlPluginEditor::createInterface(KActionCollection *ac, QObject *parent)
+{
+    InsertShorturlPluginEditorInterface *interface = new InsertShorturlPluginEditorInterface(parent);
+    interface->createAction(ac);
+    return interface;
+}
+
+#include "insertshorturlplugineditor.moc"
---
+++ kmail/editorplugins/insertshorturl/insertshorturlplugineditorinterface.cpp
@@ -0,0 +1,58 @@
+/*
+   Copyright (C) 2017 Montel Laurent <montel@kde.org>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; see the file COPYING.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "insertshorturlplugineditorinterface.h"
+#include <KPIMTextEdit/RichTextEditor>
+#include <KLocalizedString>
+#include <KActionCollection>
+#include <QAction>
+
+InsertShorturlPluginEditorInterface::InsertShorturlPluginEditorInterface(QObject *parent)
+    : MessageComposer::PluginEditorInterface(parent)
+{
+}
+
+InsertShorturlPluginEditorInterface::~InsertShorturlPluginEditorInterface()
+{
+}
+
+void InsertShorturlPluginEditorInterface::createAction(KActionCollection *ac)
+{
+    /*
+    QAction *action = new QAction(i18n("Insert Email..."), this);
+    ac->addAction(QStringLiteral("insert_email"), action);
+    connect(action, &QAction::triggered, this, &InsertShorturlPluginEditorInterface::slotActivated);
+    MessageComposer::ActionType type(action, MessageComposer::ActionType::Edit);
+    setActionType(type);
+    */
+}
+
+void InsertShorturlPluginEditorInterface::slotActivated()
+{
+    Q_EMIT emitPluginActivated(this);
+}
+
+void InsertShorturlPluginEditorInterface::exec()
+{
+}
+
+//void InsertShorturlPluginEditorInterface::slotEmailSelected(const QStringList &lst)
+//{
+//    richTextEditor()->insertPlainText(lst.join(QLatin1Char(' ')));
+//}
