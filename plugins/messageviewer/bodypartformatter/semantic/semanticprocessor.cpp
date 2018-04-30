f3964e5a945c4bb41c6665d9aa6e609b62b1e371
Volker Krause <vkrause@kde.org>
2018-04-30 17:12:28 +0200

    Add support for the new PDF extractors

--- plugins/messageviewer/bodypartformatter/semantic/semanticprocessor.cpp
+++ plugins/messageviewer/bodypartformatter/semantic/semanticprocessor.cpp
@@ -25,6 +25,7 @@
 #include <KItinerary/ExtractorPreprocessor>
 #include <KItinerary/ExtractorPostprocessor>
 #include <KItinerary/JsonLdDocument>
+#include <KItinerary/PdfDocument>
 #include <KItinerary/StructuredDataExtractor>
 
 #include <KPkPass/Pass>
@@ -102,20 +103,22 @@ MimeTreeParser::MessagePart::Ptr SemanticProcessor::process(MimeTreeParser::Inte
     qCDebug(SEMANTIC_LOG) << "Found unstructured extractor rules for message" << extractors.size();
 
     ExtractorPreprocessor preproc;
+    std::unique_ptr<PdfDocument> pdfDoc;
     if (part.content()->contentType()->isPlainText()) {
         preproc.preprocessPlainText(part.content()->decodedText());
     } else if (part.content()->contentType()->isHTMLText()) {
         preproc.preprocessHtml(part.content()->decodedText());
     } else if (part.content()->contentType()->mimeType() == "application/pdf") {
-        preproc.preprocessPdf(part.content()->decodedContent());
+        pdfDoc.reset(PdfDocument::fromData(part.content()->decodedContent()));
     }
 
+    ExtractorEngine engine;
+    engine.setSenderDate(static_cast<KMime::Message *>(part.content()->topLevel())->date()->dateTime());
+    engine.setText(preproc.text());
+    engine.setPass(pass.get());
+    engine.setPdfDocument(pdfDoc.get());
     for (auto extractor : extractors) {
-        ExtractorEngine engine;
         engine.setExtractor(extractor);
-        engine.setSenderDate(static_cast<KMime::Message *>(part.content()->topLevel())->date()->dateTime());
-        engine.setText(preproc.text());
-        engine.setPass(pass.get());
         const auto data = engine.extract();
         qCDebug(SEMANTIC_LOG).noquote() << QJsonDocument(data).toJson();
         const auto decodedData = JsonLdDocument::fromJson(data);
