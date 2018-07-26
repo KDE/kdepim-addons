dc10efe36ab63e55deb18b20b55f94bbee4a41f1
Laurent Montel <montel@kde.org>
2018-07-26 08:43:25 +0200

    Fix Bug 396685 - Edit Location not possible with dark theme
    BUG:396685
    FIXED-IN: 5.9.0

--- kaddressbook/contacteditor/addresseditor/webengine/addresseslocationenginepage.cpp
+++ kaddressbook/contacteditor/addresseditor/webengine/addresseslocationenginepage.cpp
@@ -55,6 +55,10 @@ bool AddressesLocationEnginePage::acceptNavigationRequest(const QUrl &url, Navig
 {
     Q_UNUSED(type);
     Q_UNUSED(isMainFrame);
+    if (url.scheme() == QLatin1String("data")) {
+        return true;
+    }
+
     Q_EMIT urlClicked(url);
     return false;
 }
