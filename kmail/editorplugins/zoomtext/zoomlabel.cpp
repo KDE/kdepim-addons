f929d38353d68b685acacae1f1b6caef8b85f10b
Laurent Montel <montel@kde.org>
2018-08-06 13:35:59 +0200

    Increase version

---
+++ kmail/editorplugins/zoomtext/zoomlabel.cpp
@@ -0,0 +1,6 @@
+#include "zoomlabel.h"
+
+ZoomLabel::ZoomLabel()
+{
+
+}
---
+++ kmail/editorplugins/zoomtext/zoomlabel.h
@@ -0,0 +1,11 @@
+#ifndef ZOOMLABEL_H
+#define ZOOMLABEL_H
+
+
+class ZoomLabel : public QLabel
+{
+public:
+    ZoomLabel();
+};
+
+#endif // ZOOMLABEL_H
