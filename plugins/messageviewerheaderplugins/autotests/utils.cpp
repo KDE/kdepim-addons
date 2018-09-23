bbb1fad2402ca931c815615e65b871ba58b5d581
Sandro Knauß <sknauss@kde.org>
2018-09-23 15:49:55 +0200

    prepare messageviewerheaderplugins for real header tests.

---
+++ plugins/messageviewerheaderplugins/autotests/utils.cpp
@@ -0,0 +1,93 @@
+/*
+   Copyright (C) 2015-2018 Montel Laurent <montel@kde.org>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; see the file COPYING.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "utils.h"
+#include <QFile>
+#include <QTest>
+#include <QStandardPaths>
+#include <QProcess>
+
+#include <KMime/Message>
+#include <MessageViewer/HeaderStyle>
+
+void testHeaderFile(const QString &data, const QString &name)
+{
+    QString header = QStringLiteral("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n"
+                                    "<html xmlns=\"http://www.w3.org/1999/xhtml\">\n"
+                                    "<body>\n");
+    header += data;
+    header += QStringLiteral("\n</body>\n</html>\n");
+
+    QString imagePath(QStandardPaths::locate(QStandardPaths::GenericDataLocation, QStringLiteral("libmessageviewer/pics"), QStandardPaths::LocateDirectory));
+    header.replace(QStringLiteral("file://") + imagePath, QStringLiteral("file://PATHTOIMAGES"));
+    header.replace(QRegExp(QStringLiteral("[\t ]+")), QStringLiteral(" "));
+    header.replace(QRegExp(QStringLiteral("[\t ]*\n+[\t ]*")), QStringLiteral("\n"));
+    header.replace(QRegExp(QStringLiteral("([\n\t ])\\1+")), QStringLiteral("\\1"));
+    header.replace(QRegExp(QStringLiteral(">\n+[\t ]*")), QStringLiteral(">"));
+    header.replace(QRegExp(QStringLiteral("[\t ]*\n+[\t ]*<")), QStringLiteral("<"));
+    header.replace(QLatin1String("&nbsp;"), QLatin1String("NBSP_ENTITY_PLACEHOLDER")); // xmlling chokes on &nbsp;
+
+    QString outName = name + QStringLiteral(".out.html");
+    QString fName = name + QStringLiteral(".html");
+
+    QVERIFY(QFile(QStringLiteral(HEADER_DATA_DIR "/") + fName).exists());
+
+    {
+        QFile f(outName);
+        f.open(QIODevice::WriteOnly);
+        f.write(header.toUtf8());
+        f.close();
+    }
+    // TODO add proper cmake check for xmllint and diff
+    {
+        const QStringList args = QStringList()
+                                 << QStringLiteral("--format")
+                                 << QStringLiteral("--encode") << QStringLiteral("UTF8")
+                                 << QStringLiteral("--output") << fName
+                                 << outName;
+        QCOMPARE(QProcess::execute(QStringLiteral("xmllint"), args), 0);
+    }
+
+    {
+        // compare to reference file
+        const QStringList args = QStringList()
+                                 << QStringLiteral("-u")
+                                 << fName
+                                 << QStringLiteral(HEADER_DATA_DIR "/") + fName;
+        QProcess proc;
+        proc.setProcessChannelMode(QProcess::ForwardedChannels);
+        proc.start(QStringLiteral("diff"), args);
+        QVERIFY(proc.waitForFinished());
+
+        QCOMPARE(proc.exitCode(), 0);
+    }
+}
+
+KMime::Message::Ptr readAndParseMail(const QString &mailFile)
+{
+    QFile file(QStringLiteral(HEADER_DATA_DIR) + QLatin1Char('/') + mailFile);
+    bool openFile = file.open(QIODevice::ReadOnly);
+    Q_ASSERT(openFile);
+    const QByteArray data = KMime::CRLFtoLF(file.readAll());
+    Q_ASSERT(!data.isEmpty());
+    KMime::Message::Ptr msg(new KMime::Message);
+    msg->setContent(data);
+    msg->parse();
+    return msg;
+}
