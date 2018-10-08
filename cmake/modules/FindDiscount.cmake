36efa79f964d2e8110c90fdc08fd41155779b1b8
Laurent Montel <laurent.montel@kdab.com>
2018-10-08 13:12:55 +0200

    Fix search discount lib

--- cmake/modules/FindDiscount.cmake
+++ cmake/modules/FindDiscount.cmake
@@ -13,14 +13,14 @@
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
 
-if (discount_INCLUDE_DIR AND discount_LIBRARIES)
+if (discount_INCLUDE_DIRS AND discount_LIBRARIES)
 
   # Already in cache
   set (discount_FOUND TRUE)
 
-else (discount_INCLUDE_DIR AND discount_LIBRARIES)
+else (discount_INCLUDE_DIRS AND discount_LIBRARIES)
 
-  find_library (discount_LIBRARIES
+  find_library (discount_LIBRARY
     NAMES markdown libmarkdown
   )
 
@@ -28,14 +28,16 @@ else (discount_INCLUDE_DIR AND discount_LIBRARIES)
     NAMES mkdio.h
   )
 
+  set(discount_LIBRARIES ${discount_LIBRARY})
+  set(discount_INCLUDE_DIRS ${discount_INCLUDE_DIR})
+  
   include (FindPackageHandleStandardArgs)
   find_package_handle_standard_args (discount DEFAULT_MSG discount_LIBRARIES discount_INCLUDE_DIR)
 
-endif (discount_INCLUDE_DIR AND discount_LIBRARIES)
+endif (discount_INCLUDE_DIRS AND discount_LIBRARIES)
 
-mark_as_advanced(discount_INCLUDE_DIR discount_LIBRARIES)
-
+mark_as_advanced(discount_INCLUDE_DIRS discount_LIBRARIES)
 if (discount_FOUND)
    add_library(discount::Lib UNKNOWN IMPORTED)
-   set_target_properties(discount::Lib PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${discount_INCLUDE_DIR} IMPORTED_LOCATION ${discount_LIBRARIES})
+   set_target_properties(discount::Lib PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${discount_INCLUDE_DIRS} IMPORTED_LOCATION ${discount_LIBRARIES})
 endif()
