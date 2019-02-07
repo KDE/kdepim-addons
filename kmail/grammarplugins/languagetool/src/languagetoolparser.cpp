8a0d4bbfbeb8d54ee09830acab5742aded57044e
Laurent Montel <montel@kde.org>
2019-02-07 22:39:59 +0100

    Prepare to implement parser

---
+++ kmail/grammarplugins/languagetool/src/languagetoolparser.cpp
@@ -0,0 +1,6 @@
+#include "languagetoolparser.h"
+
+LanguageToolParser::LanguageToolParser()
+{
+
+}
---
+++ kmail/grammarplugins/languagetool/src/languagetoolparser.h
@@ -0,0 +1,11 @@
+#ifndef LANGUAGETOOLPARSER_H
+#define LANGUAGETOOLPARSER_H
+
+
+class LanguageToolParser
+{
+public:
+    LanguageToolParser();
+};
+
+#endif // LANGUAGETOOLPARSER_H
