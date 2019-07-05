b572f45e5642ecca6d33ca2f27cac5fe0a562fa9
Pino Toscano <pino@kde.org>
2019-07-05 10:48:04 +0200

    i18n: use rc.cpp for extra sources
    Use rc.cpp as only dummy source for non-C strings, so repack-pot is
    happier.

--- plugins/messageviewer/bodypartformatter/pkpass/Messages.sh
+++ plugins/messageviewer/bodypartformatter/pkpass/Messages.sh
@@ -1,4 +1,3 @@
 #! /bin/sh
-$EXTRACT_GRANTLEE_TEMPLATE_STRINGS `find templates -name \*.html` >> html.cpp
-$XGETTEXT html.cpp *.cpp -o $podir/messageviewer_bodypartformatter_pkpass.pot
-rm -f html.cpp
+$EXTRACT_GRANTLEE_TEMPLATE_STRINGS `find templates -name \*.html` >> rc.cpp
+$XGETTEXT *.cpp -o $podir/messageviewer_bodypartformatter_pkpass.pot
--- plugins/messageviewer/bodypartformatter/semantic/Messages.sh
+++ plugins/messageviewer/bodypartformatter/semantic/Messages.sh
@@ -1,4 +1,3 @@
 #! /bin/sh
-$EXTRACT_GRANTLEE_TEMPLATE_STRINGS `find templates -name \*.html` >> html.cpp
+$EXTRACT_GRANTLEE_TEMPLATE_STRINGS `find templates -name \*.html` >> rc.cpp
 $XGETTEXT *.cpp -o $podir/messageviewer_semantic_plugin.pot
-rm -f html.cpp
