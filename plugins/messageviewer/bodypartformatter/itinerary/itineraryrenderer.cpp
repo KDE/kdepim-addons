194a21c7687bcc2319c42798432e53f72046349e
Volker Krause <vkrause@kde.org>
2019-08-17 18:18:12 +0200

    Rename itinerary templates
    GIT_SILENT

--- plugins/messageviewer/bodypartformatter/itinerary/itineraryrenderer.cpp
+++ plugins/messageviewer/bodypartformatter/itinerary/itineraryrenderer.cpp
@@ -207,7 +207,7 @@ bool ItineraryRenderer::render(const MimeTreeParser::MessagePartPtr &msgPart, Me
     }
     c.insert(QStringLiteral("data"), elems);
 
-    auto t = MessageViewer::MessagePartRendererManager::self()->loadByName(QStringLiteral("org.kde.messageviewer/semantic/semantic.html"));
+    auto t = MessageViewer::MessagePartRendererManager::self()->loadByName(QStringLiteral("org.kde.messageviewer/itinerary/itinerary.html"));
     const_cast<Grantlee::Engine *>(t->engine())->addDefaultLibrary(QStringLiteral("kitinerary_grantlee_extension"));
     Grantlee::OutputStream s(htmlWriter->stream());
     t->render(&s, &c);
