ad08c4ade4651c574b3aca095542ace6674e042f
Laurent Montel <montel@kde.org>
2020-04-06 14:00:58 +0200

    React when we click on cancel

--- sieveeditor/imapfoldercompletion/selectimapfolderwidget.cpp
+++ sieveeditor/imapfoldercompletion/selectimapfolderwidget.cpp
@@ -119,7 +119,11 @@ void SelectImapFolderWidget::createFolder()
 {
     const QModelIndex index = mTreeView->currentIndex();
     if (index.isValid()) {
-        const QString name = QInputDialog::getText(this, i18n("Create Folder"), i18n("Folder Name:"));
+        bool ok = false;
+        const QString name = QInputDialog::getText(this, i18n("Create Folder"), i18n("Folder Name:"), {}, {}, &ok);
+	if (!ok) {
+           return;
+	}
         if (!name.trimmed().isEmpty()) {
             const QString currentPath = index.data(SelectImapLoadFoldersJob::PathRole).toString();
             if (name.contains(QLatin1Char('/'))) {
