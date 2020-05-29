5841e23f7a51d4cdfbc5c3a1ccd4a14c40d1dae7
Laurent Montel <montel@kde.org>
2020-05-29 06:55:50 +0200

    Fix potential problem with qt5.15

--- kmail/editorconvertertextplugins/markdown/markdownlib/markdownenginepage.cpp
+++ kmail/editorconvertertextplugins/markdown/markdownlib/markdownenginepage.cpp
@@ -34,7 +34,7 @@ MarkdownEnginePage::MarkdownEnginePage(QObject *parent)
     settings()->setAttribute(QWebEngineSettings::XSSAuditingEnabled, false);
     settings()->setAttribute(QWebEngineSettings::ErrorPageEnabled, false);
     settings()->setAttribute(QWebEngineSettings::LocalContentCanAccessRemoteUrls, false);
-    settings()->setAttribute(QWebEngineSettings::LocalContentCanAccessFileUrls, false);
+    //settings()->setAttribute(QWebEngineSettings::LocalContentCanAccessFileUrls, false);
     settings()->setAttribute(QWebEngineSettings::ScreenCaptureEnabled, false);
     settings()->setAttribute(QWebEngineSettings::WebGLEnabled, false);
     settings()->setAttribute(QWebEngineSettings::AutoLoadIconsForPage, false);
