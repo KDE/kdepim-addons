86facbe27851d68b9da7fd0c53468ecffd260432
Laurent Montel <montel@kde.org>
2020-08-03 13:17:18 +0200

    Implement import windows contact

--- kaddressbook/importexportplugins/windows-contacts/windowscontactimportexportplugininterface.cpp
+++ kaddressbook/importexportplugins/windows-contacts/windowscontactimportexportplugininterface.cpp
@@ -45,17 +45,18 @@ WindowsContactImportExportPluginInterface::~WindowsContactImportExportPluginInte
 
 void WindowsContactImportExportPluginInterface::createAction(KActionCollection *ac)
 {
-    QAction *action = ac->addAction(QStringLiteral("file_export_windows_contact"));
-    action->setText(i18n("Export Windows Contact file..."));
-    action->setWhatsThis(i18n("Export contacts to windows contact file."));
-    setExportActions(QList<QAction *>() << action);
-    connect(action, &QAction::triggered, this, &WindowsContactImportExportPluginInterface::slotExportWindowsContact);
-
-    action = ac->addAction(QStringLiteral("file_import_windows_contact"));
+    QAction *action = ac->addAction(QStringLiteral("file_import_windows_contact"));
     action->setText(i18n("Import Windows Contact file..."));
     action->setWhatsThis(i18n("Import contacts from windows contact file."));
     setImportActions(QList<QAction *>() << action);
     connect(action, &QAction::triggered, this, &WindowsContactImportExportPluginInterface::slotImportWindowsContact);
+
+    //If a day we implement it
+//    action = ac->addAction(QStringLiteral("file_export_windows_contact"));
+//    action->setText(i18n("Export Windows Contact file..."));
+//    action->setWhatsThis(i18n("Export contacts to windows contact file."));
+//    setExportActions(QList<QAction *>() << action);
+//    connect(action, &QAction::triggered, this, &WindowsContactImportExportPluginInterface::slotExportWindowsContact);
 }
 
 void WindowsContactImportExportPluginInterface::slotExportWindowsContact()
@@ -90,6 +91,7 @@ void WindowsContactImportExportPluginInterface::importWindowsContact()
 
 void WindowsContactImportExportPluginInterface::exportWindowsContact()
 {
+    qWarning() << "Export windows contact not implement yet";
     //TODO
 }
 
