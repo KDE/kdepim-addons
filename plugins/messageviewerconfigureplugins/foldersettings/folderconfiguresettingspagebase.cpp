60c2a1510b6d1aacabc6c63c78059229cd21228f
Laurent Montel <montel@kde.org>
2020-09-26 15:29:53 +0200

    Enable/disable mainwidget

--- plugins/messageviewerconfigureplugins/foldersettings/folderconfiguresettingspagebase.cpp
+++ plugins/messageviewerconfigureplugins/foldersettings/folderconfiguresettingspagebase.cpp
@@ -6,13 +6,40 @@
 
 #include "folderconfiguresettingspagebase.h"
 
+#include <KLocalizedString>
+
+#include <QCheckBox>
+#include <QVBoxLayout>
+
 FolderConfigureSettingsPageBase::FolderConfigureSettingsPageBase(QWidget *parent)
     : QWidget(parent)
+    , mMainLayout(new QVBoxLayout(this))
 {
-
+    mMainLayout->setObjectName(QStringLiteral("mMainLayout"));
+    mModifyCheckBox = new QCheckBox(i18n("Modify Settings"), this);
+    mModifyCheckBox->setObjectName(QStringLiteral("mModifiedCheckBox"));
+    mModifyCheckBox->setChecked(false);
+    connect(mModifyCheckBox, &QCheckBox::clicked, this, &FolderConfigureSettingsPageBase::slotModifyClicked);
 }
 
 FolderConfigureSettingsPageBase::~FolderConfigureSettingsPageBase()
 {
 
+}
+
+void FolderConfigureSettingsPageBase::slotModifyClicked(bool clicked)
+{
+    mMainWidget->setEnabled(clicked);
+}
+
+bool FolderConfigureSettingsPageBase::wasModified() const
+{
+    return mModifyCheckBox->isChecked();
+}
+
+void FolderConfigureSettingsPageBase::addMainWidget(QWidget *widget)
+{
+    mMainWidget = widget;
+    mMainLayout->addWidget(mMainWidget);
+    mMainWidget->setEnabled(false); //Disable it directly
 }
--- plugins/messageviewerconfigureplugins/foldersettings/folderconfiguresettingspagebase.h
+++ plugins/messageviewerconfigureplugins/foldersettings/folderconfiguresettingspagebase.h
@@ -8,13 +8,23 @@
 #define FOLDERCONFIGURESETTINGSPAGEBASE_H
 
 #include <QWidget>
-
+class QVBoxLayout;
+class QCheckBox;
 class FolderConfigureSettingsPageBase : public QWidget
 {
     Q_OBJECT
 public:
     explicit FolderConfigureSettingsPageBase(QWidget *parent = nullptr);
     ~FolderConfigureSettingsPageBase();
+    Q_REQUIRED_RESULT bool wasModified() const;
+protected:
+    void addMainWidget(QWidget *widget);
+
+    QVBoxLayout *const mMainLayout;
+private:
+    void slotModifyClicked(bool clicked);
+    QCheckBox *mModifyCheckBox = nullptr;
+    QWidget *mMainWidget = nullptr;
 };
 
 #endif // FOLDERCONFIGURESETTINGSPAGEBASE_H
