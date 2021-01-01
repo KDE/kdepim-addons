4199100c0ffa21b5b7844a55022ffa35fa9ae60a
Laurent Montel <montel@kde.org>
2021-01-01 14:55:12 +0100

    GIT_SILENT: happy new year

--- kmail/editorconvertertextplugins/markdown/config-markdownplugin.h.cmake
+++ kmail/editorconvertertextplugins/markdown/config-markdownplugin.h.cmake
@@ -1,5 +1,5 @@
 /*
-   SPDX-FileCopyrightText: 2019-2020 Laurent Montel <montel@kde.org>
+   SPDX-FileCopyrightText: 2019-2021 Laurent Montel <montel@kde.org>
 
    SPDX-License-Identifier: LGPL-2.0-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
 */
