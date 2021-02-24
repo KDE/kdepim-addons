adbacce97ac3bbaccedba57280e44fc12d2ed074
Laurent Montel <montel@kde.org>
2021-02-24 08:43:32 +0100

    Fix warning

--- kmail/plugins/mailmergeplugin/tests/mailmergewidgettest_gui.cpp
+++ kmail/plugins/mailmergeplugin/tests/mailmergewidgettest_gui.cpp
@@ -20,8 +20,6 @@ int main(int argc, char **argv)
     aboutData.setShortDescription(i18n("Test for mailmerge widget"));
     QCommandLineParser parser;
     KAboutData::setApplicationData(aboutData);
-    parser.addVersionOption();
-    parser.addHelpOption();
     aboutData.setupCommandLine(&parser);
     parser.process(app);
     aboutData.processCommandLine(&parser);
