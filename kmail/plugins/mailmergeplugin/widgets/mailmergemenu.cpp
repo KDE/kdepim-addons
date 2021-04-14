23098f07690b4e78c0c50c18bf94add2123734d0
Laurent Montel <montel@kde.org>
2021-04-14 08:41:19 +0200

    Start to implement mail merge menu

---
+++ kmail/plugins/mailmergeplugin/widgets/mailmergemenu.cpp
@@ -0,0 +1,16 @@
+/*
+  SPDX-FileCopyrightText: 2021 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-only
+*/
+
+#include "mailmergemenu.h"
+
+MailMergeMenu::MailMergeMenu(QWidget *parent)
+    : QMenu(parent)
+{
+}
+
+MailMergeMenu::~MailMergeMenu()
+{
+}
---
+++ kmail/plugins/mailmergeplugin/widgets/mailmergemenu.h
@@ -0,0 +1,16 @@
+/*
+  SPDX-FileCopyrightText: 2021 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-only
+*/
+#pragma once
+
+#include <QMenu>
+
+class MailMergeMenu : public QMenu
+{
+    Q_OBJECT
+public:
+    explicit MailMergeMenu(QWidget *parent = nullptr);
+    ~MailMergeMenu() override;
+};
