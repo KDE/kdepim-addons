dd09771d44f73fe54b302fb50e74d1f37b802212
Laurent Montel <montel@kde.org>
2021-06-30 19:34:12 +0200

    Prepare dialogbox

--- kmail/checkbeforedeletemailplugins/confirmbeforedeleting/confirmbeforedeletingcreateruledialog.cpp
+++ kmail/checkbeforedeletemailplugins/confirmbeforedeleting/confirmbeforedeletingcreateruledialog.cpp
@@ -5,10 +5,25 @@
 */
 
 #include "confirmbeforedeletingcreateruledialog.h"
+#include <KLocalizedString>
+#include <QDialogButtonBox>
+#include <QVBoxLayout>
 
 ConfirmBeforeDeletingCreateRuleDialog::ConfirmBeforeDeletingCreateRuleDialog(QWidget *parent)
     : QDialog(parent)
 {
+    setWindowTitle(i18nc("@title:window", "Configure Rule"));
+    auto mainLayout = new QVBoxLayout(this);
+    mainLayout->setObjectName(QStringLiteral("mainlayout"));
+
+    //    mConfirmBeforeDeletingWidget->setObjectName(QStringLiteral("mConfirmBeforeDeletingWidget"));
+    //    mainLayout->addWidget(mConfirmBeforeDeletingWidget);
+
+    auto buttonBox = new QDialogButtonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel, this);
+    buttonBox->setObjectName(QStringLiteral("buttonBox"));
+    mainLayout->addWidget(buttonBox);
+    connect(buttonBox, &QDialogButtonBox::accepted, this, &ConfirmBeforeDeletingCreateRuleDialog::accept);
+    connect(buttonBox, &QDialogButtonBox::rejected, this, &ConfirmBeforeDeletingCreateRuleDialog::reject);
 }
 
 ConfirmBeforeDeletingCreateRuleDialog::~ConfirmBeforeDeletingCreateRuleDialog()
