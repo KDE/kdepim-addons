f145ea470619c0121a0b13bcbbaedc2c86de2dcf
Laurent Montel <montel@kde.org>
2021-07-05 08:36:02 +0200

    Not necessary to use QPointer

--- kmail/checkbeforedeletemailplugins/confirmbeforedeleting/confirmbeforedeletingplugin.cpp
+++ kmail/checkbeforedeletemailplugins/confirmbeforedeleting/confirmbeforedeletingplugin.cpp
@@ -7,7 +7,6 @@
 #include "confirmbeforedeletingplugin.h"
 #include "confirmbeforedeletingdialog.h"
 #include <KPluginFactory>
-#include <QPointer>
 
 K_PLUGIN_CLASS_WITH_JSON(ConfirmBeforeDeletingPlugin, "kmail_confirmbeforedeletingplugin.json")
 
@@ -22,11 +21,8 @@ ConfirmBeforeDeletingPlugin::~ConfirmBeforeDeletingPlugin()
 
 void ConfirmBeforeDeletingPlugin::showConfigureDialog(QWidget *parent)
 {
-    QPointer<ConfirmBeforeDeletingDialog> dlg = new ConfirmBeforeDeletingDialog(parent);
-    if (dlg->exec()) {
-        // TODO save element
-    }
-    delete dlg;
+    ConfirmBeforeDeletingDialog dlg(parent);
+    dlg.exec();
 }
 
 #include "confirmbeforedeletingplugin.moc"
