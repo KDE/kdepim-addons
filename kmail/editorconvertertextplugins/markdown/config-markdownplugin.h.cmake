a1391f60f2177f35653e05d46ecde06f0a14679d
Laurent Montel <montel@kde.org>
2021-09-20 21:05:36 +0200

    GIT_SILENT: use pragma once here too

--- kmail/editorconvertertextplugins/markdown/config-markdownplugin.h.cmake
+++ kmail/editorconvertertextplugins/markdown/config-markdownplugin.h.cmake
@@ -4,9 +4,6 @@
    SPDX-License-Identifier: LGPL-2.0-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
 */
 
-#ifndef MARKDOWNPLUGIN_CONFIG_H
-#define MARKDOWNPLUGIN_CONFIG_H
-
+#pragma once
 #cmakedefine01 DISCOUNT_HAS_HIGHLIGHTING_SUPPORT
 
-#endif
