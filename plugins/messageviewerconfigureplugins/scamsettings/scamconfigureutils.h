169c7b10a68f0ec5adde766f19d424c1a5f0cf3a
Laurent Montel <montel@kde.org>
2021-11-01 16:13:32 +0100

    Add utils class

---
+++ plugins/messageviewerconfigureplugins/scamsettings/scamconfigureutils.h
@@ -0,0 +1,14 @@
+/*
+   SPDX-FileCopyrightText: 2021 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#pragma once
+
+#include "scamconfiguresettings_private_export.h"
+#include <QString>
+
+namespace ScamConfigureUtils
+{
+Q_REQUIRED_RESULT QString convertEnumToString();
+};
