384b429b94930acfb86f3843ba7f4b3a8587bf29
Laurent Montel <montel@kde.org>
2021-11-02 07:19:31 +0100

    Add i18n

--- plugins/messageviewerconfigureplugins/scamsettings/scamconfigureutils.cpp
+++ plugins/messageviewerconfigureplugins/scamsettings/scamconfigureutils.cpp
@@ -5,8 +5,18 @@
 */
 
 #include "scamconfigureutils.h"
+#include <KLocalizedString>
 QString ScamConfigureUtils::convertEnumToString(MessageViewer::ScamDetectionInfo::ScamDetectionField flag)
 {
-    // TODO
+    switch (flag) {
+    case MessageViewer::ScamDetectionInfo::ScamDetectionField::None:
+        break;
+    case MessageViewer::ScamDetectionInfo::ScamDetectionField::DisableAll:
+        return i18n("Disable All Check");
+    case MessageViewer::ScamDetectionInfo::ScamDetectionField::RedirectUrl:
+        return i18n("Redirect Url");
+    case MessageViewer::ScamDetectionInfo::ScamDetectionField::HasIp:
+        return i18n("Has Ip");
+    }
     return {};
 }
