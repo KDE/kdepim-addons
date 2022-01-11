9244b004a11f00c46d55e015c069cbe2f4ddb216
Laurent Montel <montel@kde.org>
2022-01-11 13:30:56 +0100

    Start to implement new plugin: allow to open url in specific apps (as
    zoom/ruqola etc.)

---
+++ plugins/messageviewerconfigureplugins/openurlwith/openurlwithconfigplugin.cpp
@@ -0,0 +1,26 @@
+/*
+   SPDX-FileCopyrightText: 2022 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "openurlwithconfigplugin.h"
+
+#include <KPluginFactory>
+
+K_PLUGIN_CLASS_WITH_JSON(OpenUrlWithConfigPlugin, "messageviewer_openurlwithconfigplugin.json")
+
+OpenUrlWithConfigPlugin::OpenUrlWithConfigPlugin(QObject *parent, const QList<QVariant> &)
+    : MessageViewer::MessageViewerConfigureSettingsPlugin(parent)
+{
+}
+
+OpenUrlWithConfigPlugin::~OpenUrlWithConfigPlugin() = default;
+
+void OpenUrlWithConfigPlugin::showConfigureDialog(QWidget *parent)
+{
+//    DKIMConfigureDialog dlg(parent);
+//    dlg.exec();
+}
+
+#include "openurlwithconfigplugin.moc"
---
+++ plugins/messageviewerconfigureplugins/openurlwith/openurlwithconfigplugin.h
@@ -0,0 +1,22 @@
+/*
+   SPDX-FileCopyrightText: 2022 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#pragma once
+
+
+#include <MessageViewer/MessageViewerConfigureSettingsPlugin>
+
+#include <QVariant>
+
+class OpenUrlWithConfigPlugin : public MessageViewer::MessageViewerConfigureSettingsPlugin
+{
+    Q_OBJECT
+public:
+    explicit OpenUrlWithConfigPlugin(QObject *parent = nullptr, const QList<QVariant> & = QList<QVariant>());
+    ~OpenUrlWithConfigPlugin() override;
+
+    void showConfigureDialog(QWidget *parent) override;
+};
+
