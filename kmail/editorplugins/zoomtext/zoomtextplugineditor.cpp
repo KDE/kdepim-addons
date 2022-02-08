4fbaeb8e501914bd63e716cdac0e8ad810de8160
Laurent Montel <montel@kde.org>
2022-02-08 13:32:06 +0100

    Fix Bug 434335 Zoom in/out is missing in the context menu
    BUG: 434335
    FIXED-IN: 5.19.3

--- kmail/editorplugins/zoomtext/zoomtextplugineditor.cpp
+++ kmail/editorplugins/zoomtext/zoomtextplugineditor.cpp
@@ -27,7 +27,7 @@ MessageComposer::PluginEditorInterface *ZoomTextPluginEditor::createInterface(QO
 
 bool ZoomTextPluginEditor::hasPopupMenuSupport() const
 {
-    return false;
+    return true;
 }
 
 bool ZoomTextPluginEditor::hasStatusBarSupport() const
