e69545b7003b5c88df6879858f9bd398e3b801cb
Laurent Montel <montel@kde.org>
2022-03-29 07:35:43 +0200

    Adapt include for 5.93

--- kmail/grammarplugins/grammarcommon/grammarresulttextedit.cpp
+++ kmail/grammarplugins/grammarcommon/grammarresulttextedit.cpp
@@ -6,6 +6,10 @@
 
 #include "grammarresulttextedit.h"
 #include "grammarcommon_debug.h"
+#include <kconfigwidgets_version.h>
+#if KCONFIGWIDGETS_VERSION >= QT_VERSION_CHECK(5, 93, 0)
+#include <KStatefulBrush> // was moved to own header in 5.93.0
+#endif
 #include <MessageComposer/PluginEditorGrammarCustomToolsViewInterface>
 
 #include <KLocalizedString>
