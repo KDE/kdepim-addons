d54d5510e21fbacfbeb3b244fcbd3326706cc752
Laurent Montel <montel@kde.org>
2022-05-16 07:50:59 +0200

    We depend against kf5.94 now

--- kmail/grammarplugins/grammarcommon/grammarresulttextedit.cpp
+++ kmail/grammarplugins/grammarcommon/grammarresulttextedit.cpp
@@ -6,10 +6,7 @@
 
 #include "grammarresulttextedit.h"
 #include "grammarcommon_debug.h"
-#include <kconfigwidgets_version.h>
-#if KCONFIGWIDGETS_VERSION >= QT_VERSION_CHECK(5, 93, 0)
-#include <KStatefulBrush> // was moved to own header in 5.93.0
-#endif
+#include <KStatefulBrush>
 #include <MessageComposer/PluginEditorGrammarCustomToolsViewInterface>
 
 #include <KLocalizedString>
--- sieveeditor/emaillineedit/emaillineedit.cpp
+++ sieveeditor/emaillineedit/emaillineedit.cpp
@@ -6,12 +6,9 @@
 
 #include "emaillineedit.h"
 #include "sieveeditoremaillineditplugin_debug.h"
-#include <kconfigwidgets_version.h>
-#if KCONFIGWIDGETS_VERSION >= QT_VERSION_CHECK(5, 93, 0)
-#include <KStatefulBrush> // was moved to own header in 5.93.0
-#endif
 #include <Akonadi/EmailAddressSelectionDialog>
 #include <KPluginFactory>
+#include <KStatefulBrush>
 
 #include <KColorScheme>
 #include <KLocalizedString>
