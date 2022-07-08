9b9bc8293e360562cc884fb4843c914e018cf11a
Laurent Montel <montel@kde.org>
2022-07-08 08:21:52 +0200

    Add more autotest

---
+++ kmail/plugins/akonadidatabasetools/autotests/akonadidatabasetoolswidgettest.cpp
@@ -0,0 +1,27 @@
+/*
+   SPDX-FileCopyrightText: 2022 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#include "akonadidatabasetoolswidgettest.h"
+#include "akonadidatabasetoolswidget.h"
+#include <QPlainTextEdit>
+#include <QTest>
+#include <QVBoxLayout>
+QTEST_MAIN(AkonadiDatabaseToolsWidgetTest)
+AkonadiDatabaseToolsWidgetTest::AkonadiDatabaseToolsWidgetTest(QObject *parent)
+    : QObject{parent}
+{
+}
+
+void AkonadiDatabaseToolsWidgetTest::shouldHaveDefaultValues()
+{
+    AkonadiDatabaseToolsWidget w;
+    auto mainLayout = w.findChild<QVBoxLayout *>(QStringLiteral("mainLayout"));
+    QVERIFY(mainLayout);
+    QCOMPARE(mainLayout->contentsMargins(), QMargins{});
+
+    auto mPlainTextEdit = w.findChild<QPlainTextEdit *>(QStringLiteral("mPlainTextEdit"));
+    QVERIFY(mPlainTextEdit);
+    QVERIFY(mPlainTextEdit->toPlainText().isEmpty());
+}
