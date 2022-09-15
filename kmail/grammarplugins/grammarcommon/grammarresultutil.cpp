2af8dc8ca440d3a7436084af2d624f434aecb591
Laurent Montel <montel@kde.org>
2022-09-15 09:00:33 +0200

    Continue to debug grammar support

--- kmail/grammarplugins/grammarcommon/grammarresultutil.cpp
+++ kmail/grammarplugins/grammarcommon/grammarresultutil.cpp
@@ -56,23 +56,57 @@ void GrammarResultUtil::replaceWord(const MessageComposer::PluginGrammarAction &
         cur.setPosition(position + act.length(), QTextCursor::KeepAnchor);
         QTextCharFormat format;
         cur.insertText(replacementWord, format);
-        const int diff = act.length() - replacementWord.length();
+        const int diff = replacementWord.length() - act.length();
+        // qDebug() << " diff " << diff;
         if (diff != 0) {
             const int blockLength = block.length();
             for (int i = position + replacementWord.length() + 1; i < blockLength; ++i) {
                 cur.setPosition(i);
+                // qDebug() << " XCWCWXCWCWXCWXCCX  " << i;
                 QTextCharFormat currentCharFormat = cur.charFormat();
                 if (currentCharFormat.hasProperty(GrammarResultUtil::TextInfo::ReplaceFormatInfo)) {
                     auto act = cur.charFormat().property(GrammarResultUtil::TextInfo::ReplaceFormatInfo).value<MessageComposer::PluginGrammarAction>();
-                    act.setStart(act.start() - diff);
+                    // qDebug() << "BEFORE DDDDDDDDDDDDDDDD" << act.start();
+                    act.setStart(act.start() + diff);
+                    // qDebug() << "AFTER DDDDDDDDDDDDDDDD" << act.start();
                     currentCharFormat.setProperty(GrammarResultUtil::TextInfo::ReplaceFormatInfo, QVariant::fromValue(act));
 
                     const int newPosition = i - 1;
+                    // qDebug() << " newPosition " << newPosition;
                     cur.setPosition(newPosition);
                     cur.setPosition(newPosition + act.length(), QTextCursor::KeepAnchor);
+                    // qDebug() << "2222 newPosition " << newPosition + act.length();
                     cur.setCharFormat(currentCharFormat);
 
                     i += act.length();
+                }
+            }
+            // qDebug() << " block.blockNumber() " << block.blockNumber() << " document->blockCount() " << document->blockCount();
+            for (int b = block.blockNumber() + 1; b < document->blockCount(); ++b) {
+                QTextBlock block = document->findBlockByNumber(b);
+                if (block.isValid()) {
+                    QTextCursor cur(block);
+                    const int blockLength = block.length();
+                    for (int i = 0; i < blockLength; ++i) {
+                        cur.setPosition(block.length() + i);
+                        QTextCharFormat currentCharFormat = cur.charFormat();
+                        if (currentCharFormat.hasProperty(GrammarResultUtil::TextInfo::ReplaceFormatInfo)) {
+                            auto act = cur.charFormat().property(GrammarResultUtil::TextInfo::ReplaceFormatInfo).value<MessageComposer::PluginGrammarAction>();
+                            // qDebug() << "BEFORE DDDDDDDDDDDDDDDD" << act.start();
+                            act.setStart(act.start() + diff);
+                            // qDebug() << "AFTER DDDDDDDDDDDDDDDD" << act.start();
+                            currentCharFormat.setProperty(GrammarResultUtil::TextInfo::ReplaceFormatInfo, QVariant::fromValue(act));
+
+                            const int newPosition = act.start() - 1;
+                            // qDebug() << " newPosition " << newPosition;
+                            cur.setPosition(newPosition);
+                            cur.setPosition(newPosition + act.length(), QTextCursor::KeepAnchor);
+                            // qDebug() << "2222 newPosition " << newPosition + act.length();
+                            cur.setCharFormat(currentCharFormat);
+
+                            i += act.length();
+                        }
+                    }
                 }
             }
         }
