34d3698869d52c3d5f8d3934c5afc1652167285f
Laurent Montel <montel@kde.org>
2022-09-24 17:24:27 +0200

    We depend against kf5.98

--- plugins/webengineurlinterceptor/adblock/lib/widgets/adblocksettingwidget.cpp
+++ plugins/webengineurlinterceptor/adblock/lib/widgets/adblocksettingwidget.cpp
@@ -35,12 +35,7 @@ using namespace PimCommon::ConfigureImmutableWidgetUtils;
 #include <KMessageBox>
 #include <QWhatsThisClickedEvent>
 
-#include <kio_version.h>
-#if KIO_VERSION >= QT_VERSION_CHECK(5, 98, 0)
 #include <KIO/JobUiDelegateFactory>
-#else
-#include <KIO/JobUiDelegate>
-#endif
 #include <KIO/OpenUrlJob>
 #include <QPointer>
 #include <QStandardPaths>
@@ -162,11 +157,7 @@ bool AdBlockSettingWidget::event(QEvent *event)
     if (event->type() == QEvent::WhatsThisClicked) {
         auto clicked = static_cast<QWhatsThisClickedEvent *>(event);
         auto job = new KIO::OpenUrlJob(QUrl(clicked->href()));
-#if KIO_VERSION >= QT_VERSION_CHECK(5, 98, 0)
         job->setUiDelegate(KIO::createDefaultJobUiDelegate(KJobUiDelegate::AutoHandlingEnabled, this));
-#else
-        job->setUiDelegate(new KIO::JobUiDelegate(KJobUiDelegate::AutoHandlingEnabled, this));
-#endif
         job->start();
         return true;
     }
