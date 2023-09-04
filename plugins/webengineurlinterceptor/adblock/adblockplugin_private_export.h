e008211c688c573a20c30fee1c7373099467fdfb
Laurent Montel <montel@kde.org>
2023-09-04 19:51:20 +0200

    Add private export symbol

---
+++ plugins/webengineurlinterceptor/adblock/adblockplugin_private_export.h
@@ -0,0 +1,18 @@
+/*  This file is part of the KDE project
+    SPDX-FileCopyrightText: 2023 Laurent Montel <montel@kde.org>
+
+    SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+
+#include "libadblockplugin_export.h"
+
+/* Classes which are exported only for unit tests */
+#ifdef BUILD_TESTING
+#ifndef LIBADBLOCKPLUGIN_TESTS_EXPORT
+#define LIBADBLOCKPLUGIN_TESTS_EXPORT LIBADBLOCKPLUGIN_EXPORT
+#endif
+#else /* not compiling tests */
+#define LIBADBLOCKPLUGIN_TESTS_EXPORT
+#endif
