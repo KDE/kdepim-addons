b529925890316c165efc30b853df45a6ea0c7ae9
Laurent Montel <montel@kde.org>
2023-09-07 13:59:15 +0200

    continue to implement adblock support

--- plugins/webengineurlinterceptor/adblock/adblockmanager.cpp
+++ plugins/webengineurlinterceptor/adblock/adblockmanager.cpp
@@ -8,13 +8,38 @@
 #include "adblockfilter.h"
 #include "globalsettings_webengineurlinterceptoradblock.h"
 
+#include <QFile>
+
 AdblockManager::AdblockManager(QObject *parent)
-    : QObject{parent}
+    : QObject{parent} // parsing the block lists takes some time, try to do it asynchronously
+                      // if it is not ready when it's needed, reading the future will block
+    , mAdblockInitFuture(std::async(std::launch::async,
+                                    [this] {
+                                        return createOrRestoreAdblock();
+                                    }))
+    , mAdblock(std::nullopt)
 {
     reloadConfig();
 }
 
 AdblockManager::~AdblockManager() = default;
+
+rust::Box<Adblock> AdblockManager::createOrRestoreAdblock()
+{
+    rust::Box<Adblock> adb = [] {
+#if 0
+        auto cacheLocation = adblockCacheLocation();
+        if (QFile::exists(cacheLocation)) {
+            return loadAdblock(cacheLocation.toStdString());
+        }
+        return newAdblock(AdblockFilterListsManager::filterListPath().toStdString());
+#endif
+        return newAdblock("");
+    }();
+
+    // Q_EMIT adblockInitialized();
+    return adb;
+}
 
 AdblockManager *AdblockManager::self()
 {
@@ -39,10 +64,68 @@ void AdblockManager::setAdblockFilterLists(const QList<AdblockFilter> &newAdbloc
     mAdblockFilterLists = newAdblockFilterLists;
 }
 
-bool AdblockManager::interceptRequest(const QWebEngineUrlRequestInfo &info)
+inline auto resourceTypeToString(const QWebEngineUrlRequestInfo::ResourceType type)
 {
-    // TODO
-    return false;
+    // Strings from https://docs.rs/crate/adblock/0.3.3/source/src/request.rs
+    using Type = QWebEngineUrlRequestInfo::ResourceType;
+    switch (type) {
+    case Type::ResourceTypeMainFrame:
+        return "main_frame";
+    case Type::ResourceTypeSubFrame:
+        return "sub_frame";
+    case Type::ResourceTypeStylesheet:
+        return "stylesheet";
+    case Type::ResourceTypeScript:
+        return "script";
+    case Type::ResourceTypeFontResource:
+        return "font";
+    case Type::ResourceTypeImage:
+        return "image";
+    case Type::ResourceTypeSubResource:
+        return "object_subrequest"; // TODO CHECK
+    case Type::ResourceTypeObject:
+        return "object";
+    case Type::ResourceTypeMedia:
+        return "media";
+    case Type::ResourceTypeFavicon:
+        return "image"; // almost
+    case Type::ResourceTypeXhr:
+        return "xhr";
+    case Type::ResourceTypePing:
+        return "ping";
+    case Type::ResourceTypeCspReport:
+        return "csp_report";
+    default:
+        return "other";
+    }
+}
+bool AdblockManager::interceptRequest(QWebEngineUrlRequestInfo &info)
+{
+    // Only wait for the adblock initialization if it isn't ready on first use
+    if (!mAdblock) {
+        qDebug() << "Adblock not yet initialized, blindly allowing request";
+        return false;
+    }
+
+    const std::string url = info.requestUrl().toString().toStdString();
+    const std::string firstPartyUrl = info.firstPartyUrl().toString().toStdString();
+    const AdblockResult result = (*mAdblock)->shouldBlock(url, firstPartyUrl, resourceTypeToString(info.resourceType()));
+
+    const auto &redirect = result.redirect;
+    const auto &rewrittenUrl = result.rewrittenUrl;
+    if (!redirect.empty()) {
+        info.redirect(QUrl(QString::fromStdString(std::string(redirect))));
+    } else if (result.matched) {
+        info.block(result.matched);
+    } else if (!rewrittenUrl.empty()) {
+        info.redirect(QUrl(QString::fromStdString(std::string(rewrittenUrl))));
+    }
+    return true;
 }
 
+void q_cdebug_adblock(const char *message)
+{
+    // TODO
+    // qCDebug(AdblockCategory) << message;
+}
 #include "moc_adblockmanager.cpp"
--- plugins/webengineurlinterceptor/adblock/adblockmanager.h
+++ plugins/webengineurlinterceptor/adblock/adblockmanager.h
@@ -28,11 +28,22 @@ public:
     Q_REQUIRED_RESULT QList<AdblockFilter> adblockFilterLists() const;
     void setAdblockFilterLists(const QList<AdblockFilter> &newAdblockFilterLists);
 
-    Q_REQUIRED_RESULT bool interceptRequest(const QWebEngineUrlRequestInfo &info);
+    Q_REQUIRED_RESULT bool interceptRequest(QWebEngineUrlRequestInfo &info);
 Q_SIGNALS:
     void enabledChanged(bool enabled);
 
 private:
     void reloadConfig();
+
+    /// If an adblock cache is found, loads it, otherwise creates a new adblock
+    /// from the current filter lists.
+    rust::Box<Adblock> createOrRestoreAdblock();
+
+    std::future<rust::Box<Adblock>> mAdblockInitFuture;
+    std::optional<rust::Box<Adblock>> mAdblock;
     QList<AdblockFilter> mAdblockFilterLists;
 };
+
+extern "C" {
+void q_cdebug_adblock(const char *message);
+}
