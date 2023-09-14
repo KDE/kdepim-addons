230fb32586a630196c927c9db1f323eb651043fd
Laurent Montel <montel@kde.org>
2023-09-14 07:55:02 +0200

    Add test apps

---
+++ plugins/webengineurlinterceptor/adblock/tests/adblockdialog_gui.cpp
@@ -0,0 +1,27 @@
+/*
+   SPDX-FileCopyrightText: 2023 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "adblockfilterdialog.h"
+#include <QApplication>
+#include <QCommandLineParser>
+#include <QStandardPaths>
+
+int main(int argc, char **argv)
+{
+    QApplication app(argc, argv);
+    QStandardPaths::setTestModeEnabled(true);
+    QCommandLineParser parser;
+    parser.addVersionOption();
+    parser.addHelpOption();
+    parser.process(app);
+
+    auto w = new AdblockFilterDialog;
+    w->resize(800, 600);
+    w->show();
+    app.exec();
+    delete w;
+    return 0;
+}
