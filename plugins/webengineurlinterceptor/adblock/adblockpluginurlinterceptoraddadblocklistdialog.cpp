557d6fe546a0ee6eaf49b9e8b51162b4cdadb343
Laurent Montel <montel@kde.org>
2023-09-18 07:29:10 +0200

    Prepare to add adblockpluginurlinterceptoraddadblocklistdialog

---
+++ plugins/webengineurlinterceptor/adblock/adblockpluginurlinterceptoraddadblocklistdialog.cpp
@@ -0,0 +1,18 @@
+/*
+   SPDX-FileCopyrightText: 2023 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "adblockpluginurlinterceptoraddadblocklistdialog.h"
+
+AdblockPluginUrlInterceptorAddAdblockListDialog::AdblockPluginUrlInterceptorAddAdblockListDialog(QWidget *parent)
+    : QDialog(parent)
+{
+}
+
+AdblockPluginUrlInterceptorAddAdblockListDialog::~AdblockPluginUrlInterceptorAddAdblockListDialog()
+{
+}
+
+#include "moc_adblockpluginurlinterceptoraddadblocklistdialog.cpp"
---
+++ plugins/webengineurlinterceptor/adblock/adblockpluginurlinterceptoraddadblocklistdialog.h
@@ -0,0 +1,18 @@
+/*
+   SPDX-FileCopyrightText: 2023 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#pragma once
+
+#include "adblockplugin_private_export.h"
+#include <QDialog>
+
+class LIBADBLOCKPLUGIN_TESTS_EXPORT AdblockPluginUrlInterceptorAddAdblockListDialog : public QDialog
+{
+    Q_OBJECT
+public:
+    explicit AdblockPluginUrlInterceptorAddAdblockListDialog(QWidget *parent = nullptr);
+    ~AdblockPluginUrlInterceptorAddAdblockListDialog() override;
+};
