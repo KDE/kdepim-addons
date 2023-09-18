96daa0a7759d2b294fafd7921b20dbc5cee7595a
Laurent Montel <montel@kde.org>
2023-09-18 08:31:05 +0200

    Continue to implement adblockpluginurlinterceptoraddadblocklistwidget

--- plugins/webengineurlinterceptor/adblock/adblockpluginurlinterceptoraddadblocklistwidget.cpp
+++ plugins/webengineurlinterceptor/adblock/adblockpluginurlinterceptoraddadblocklistwidget.cpp
@@ -7,12 +7,40 @@
 #include "adblockpluginurlinterceptoraddadblocklistwidget.h"
 #include <KLocalizedString>
 #include <QFormLayout>
+#include <QLineEdit>
 
 AdblockPluginUrlInterceptorAddAdblockListWidget::AdblockPluginUrlInterceptorAddAdblockListWidget(QWidget *parent)
     : QWidget{parent}
+    , mName(new QLineEdit(this))
+    , mUrl(new QLineEdit(this))
 {
+    auto mainLayout = new QFormLayout(this);
+    mainLayout->setContentsMargins({});
+
+    mName->setObjectName(QStringLiteral("mName"));
+    mUrl->setObjectName(QStringLiteral("mUrl"));
+
+    mName->setClearButtonEnabled(true);
+    mUrl->setClearButtonEnabled(true);
+
+    mainLayout->addRow(i18n("Name:"), mName);
+    mainLayout->addRow(i18n("Url:"), mUrl);
 }
 
 AdblockPluginUrlInterceptorAddAdblockListWidget::~AdblockPluginUrlInterceptorAddAdblockListWidget() = default;
+
+AdblockPluginUrlInterceptorAddAdblockListWidget::AdBlockListInfo AdblockPluginUrlInterceptorAddAdblockListWidget::info() const
+{
+    AdblockPluginUrlInterceptorAddAdblockListWidget::AdBlockListInfo i;
+    i.name = mName->text();
+    i.url = mUrl->text();
+    return i;
+}
+
+void AdblockPluginUrlInterceptorAddAdblockListWidget::setInfo(const AdBlockListInfo &adblockInfo)
+{
+    mName->setText(adblockInfo.name);
+    mUrl->setText(adblockInfo.url);
+}
 
 #include "moc_adblockpluginurlinterceptoraddadblocklistwidget.cpp"
--- plugins/webengineurlinterceptor/adblock/adblockpluginurlinterceptoraddadblocklistwidget.h
+++ plugins/webengineurlinterceptor/adblock/adblockpluginurlinterceptoraddadblocklistwidget.h
@@ -6,12 +6,25 @@
 
 #pragma once
 
+#include "adblockplugin_private_export.h"
 #include <QWidget>
-
-class AdblockPluginUrlInterceptorAddAdblockListWidget : public QWidget
+class QLineEdit;
+class LIBADBLOCKPLUGIN_TESTS_EXPORT AdblockPluginUrlInterceptorAddAdblockListWidget : public QWidget
 {
     Q_OBJECT
 public:
+    struct AdBlockListInfo {
+        QString name;
+        QString url;
+    };
+
     explicit AdblockPluginUrlInterceptorAddAdblockListWidget(QWidget *parent = nullptr);
     ~AdblockPluginUrlInterceptorAddAdblockListWidget() override;
+
+    Q_REQUIRED_RESULT AdblockPluginUrlInterceptorAddAdblockListWidget::AdBlockListInfo info() const;
+    void setInfo(const AdblockPluginUrlInterceptorAddAdblockListWidget::AdBlockListInfo &adblockInfo);
+
+private:
+    QLineEdit *const mName;
+    QLineEdit *const mUrl;
 };
