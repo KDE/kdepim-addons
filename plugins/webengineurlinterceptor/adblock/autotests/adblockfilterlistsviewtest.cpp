3175429720d9ada740742a03bc7f913e58081acb
Laurent Montel <montel@kde.org>
2023-12-30 07:44:20 +0100

    Add autotest

---
+++ plugins/webengineurlinterceptor/adblock/autotests/adblockfilterlistsviewtest.cpp
@@ -0,0 +1,21 @@
+/*
+   SPDX-FileCopyrightText: 2023 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "adblockfilterlistsviewtest.h"
+#include "adblockfilterlistsview.h"
+#include <QTest>
+QTEST_MAIN(AdblockFilterListsViewTest)
+AdblockFilterListsViewTest::AdblockFilterListsViewTest(QObject *parent)
+    : QObject{parent}
+{
+}
+
+void AdblockFilterListsViewTest::shouldHaveDefaultValues()
+{
+    AdblockFilterListsView w;
+    QVERIFY(!w.rootIsDecorated());
+    QCOMPARE(w.contextMenuPolicy(), Qt::DefaultContextMenu);
+}
