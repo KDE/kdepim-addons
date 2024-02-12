daf2123090a54dde735e784a533e872e889f574c
Laurent Montel <montel@kde.org>
2024-02-12 08:02:58 +0100

    Continue to implement model

--- plugins/messageviewerconfigureplugins/scamsettings/scamlistsmodel.cpp
+++ plugins/messageviewerconfigureplugins/scamsettings/scamlistsmodel.cpp
@@ -6,9 +6,33 @@
 
 #include "scamlistsmodel.h"
 
+#include <MessageViewer/ScamDetectionInfo>
+
 ScamListsModel::ScamListsModel(QObject *parent)
     : QAbstractListModel{parent}
 {
 }
 
 ScamListsModel::~ScamListsModel() = default;
+
+QList<MessageViewer::ScamDetectionInfo> ScamListsModel::info() const
+{
+    return mInfo;
+}
+
+void ScamListsModel::setInfo(const QList<MessageViewer::ScamDetectionInfo> &newInfo)
+{
+    beginResetModel();
+    mInfo = newInfo;
+    endResetModel();
+}
+
+int ScamListsModel::rowCount(const QModelIndex &parent) const
+{
+    if (parent.isValid()) {
+        return 0; // flat model
+    }
+    return mInfo.count();
+}
+
+#include "moc_scamlistsmodel.cpp"
--- plugins/messageviewerconfigureplugins/scamsettings/scamlistsmodel.h
+++ plugins/messageviewerconfigureplugins/scamsettings/scamlistsmodel.h
@@ -7,11 +7,22 @@
 #pragma once
 
 #include <QAbstractListModel>
-
+namespace MessageViewer
+{
+class ScamDetectionInfo;
+}
 class ScamListsModel : public QAbstractListModel
 {
     Q_OBJECT
 public:
     explicit ScamListsModel(QObject *parent = nullptr);
     ~ScamListsModel() override;
+
+    [[nodiscard]] QList<MessageViewer::ScamDetectionInfo> info() const;
+    void setInfo(const QList<MessageViewer::ScamDetectionInfo> &newInfo);
+
+    [[nodiscard]] int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+
+private:
+    QList<MessageViewer::ScamDetectionInfo> mInfo;
 };
