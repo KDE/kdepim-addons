ef7881d68e1bb23a6ffbb722ce5a8cf7f91f315d
Laurent Montel <montel@kde.org>
2025-04-22 13:30:06 +0200

    Add model

---
+++ pimautogeneratetext/src/autogenerateconfigureaskmodel.cpp
@@ -0,0 +1,33 @@
+/*
+   SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#include "autogenerateconfigureaskmodel.h"
+
+AutogenerateConfigureAskModel::AutogenerateConfigureAskModel(QObject *parent)
+    : QAbstractListModel{parent}
+{
+}
+
+AutogenerateConfigureAskModel::~AutogenerateConfigureAskModel() = default;
+
+int AutogenerateConfigureAskModel::rowCount(const QModelIndex &parent) const
+{
+    if (parent.isValid()) {
+        return 0; // flat model
+    }
+    return mAskItems.count();
+}
+
+QVariant AutogenerateConfigureAskModel::data(const QModelIndex &index, int role) const
+{
+    // TODO
+    return {};
+}
+
+bool AutogenerateConfigureAskModel::setData(const QModelIndex &index, const QVariant &value, int role)
+{
+    // TODO
+    return false;
+}
---
+++ pimautogeneratetext/src/autogenerateconfigureaskmodel.h
@@ -0,0 +1,25 @@
+/*
+   SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#pragma once
+
+#include "autogenerateconfigureaskitem.h"
+
+#include <QAbstractListModel>
+
+class AutogenerateConfigureAskModel : public QAbstractListModel
+{
+    Q_OBJECT
+public:
+    explicit AutogenerateConfigureAskModel(QObject *parent = nullptr);
+    ~AutogenerateConfigureAskModel() override;
+
+    [[nodiscard]] int rowCount(const QModelIndex & = {}) const override;
+    [[nodiscard]] QVariant data(const QModelIndex &index, int role) const override;
+    [[nodiscard]] bool setData(const QModelIndex &index, const QVariant &value, int role) override;
+
+private:
+    QList<AutogenerateConfigureAskItem> mAskItems;
+};
