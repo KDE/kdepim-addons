820b2853b123bdb20ef164ad498270905b639939
Laurent Montel <montel@kde.org>
2025-04-22 13:29:43 +0200

    Add private symbol header

---
+++ pimautogeneratetext/src/pimautogeneratetext_private_export.h
@@ -0,0 +1,18 @@
+/*  This file is part of the KDE project
+    SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+    SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+
+#include "pimautogeneratetext_export.h"
+
+/* Classes which are exported only for unit tests */
+#ifdef BUILD_TESTING
+#ifndef PIMAUTOGENERATETEXT_TESTS_EXPORT
+#define PIMAUTOGENERATETEXT_TESTS_EXPORT PIMAUTOGENERATETEXT_EXPORT
+#endif
+#else /* not compiling tests */
+#define PIMAUTOGENERATETEXT_TESTS_EXPORT
+#endif
