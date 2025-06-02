46cbb5fa5f3ae4a9d2cf830869ce3ae16c00eec0
Laurent Montel <montel@kde.org>
2025-06-02 08:15:48 +0200

    Add autogenerateanswers plugins

---
+++ kmail/editorplugins/autogenerateanswers/autogenerateanswerseditor.cpp
@@ -0,0 +1,38 @@
+/*
+   SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "autogenerateanswerseditor.h"
+#include "autogenerateanswerseditorinterface.h"
+#include <KPluginFactory>
+#include <QPointer>
+
+K_PLUGIN_CLASS_WITH_JSON(AutoGenerateAnswersEditor, "kmail_autogenerateanswerseditorplugin.json")
+
+AutoGenerateAnswersEditor::AutoGenerateAnswersEditor(QObject *parent, const QList<QVariant> &)
+    : MessageComposer::PluginEditor(parent)
+{
+}
+
+AutoGenerateAnswersEditor::~AutoGenerateAnswersEditor() = default;
+
+bool AutoGenerateAnswersEditor::hasPopupMenuSupport() const
+{
+    return false;
+}
+
+bool AutoGenerateAnswersEditor::hasConfigureDialog() const
+{
+    return false;
+}
+
+MessageComposer::PluginEditorInterface *AutoGenerateAnswersEditor::createInterface(QObject *parent)
+{
+    auto interface = new AutoGenerateAnswersEditorInterface(parent);
+    return interface;
+}
+
+#include "autogenerateanswerseditor.moc"
+#include "moc_autogenerateanswerseditor.cpp"
---
+++ kmail/editorplugins/autogenerateanswers/autogenerateanswerseditor.h
@@ -0,0 +1,26 @@
+/*
+   SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#pragma once
+
+#include <MessageComposer/PluginEditor>
+
+#include <MessageComposer/PluginEditorInterface>
+#include <QVariant>
+
+class AutoGenerateAnswersEditor : public MessageComposer::PluginEditor
+{
+    Q_OBJECT
+public:
+    explicit AutoGenerateAnswersEditor(QObject *parent = nullptr, const QList<QVariant> & = QList<QVariant>());
+    ~AutoGenerateAnswersEditor() override;
+
+    [[nodiscard]] MessageComposer::PluginEditorInterface *createInterface(QObject *parent = nullptr) override;
+    [[nodiscard]] bool hasPopupMenuSupport() const override;
+    [[nodiscard]] bool hasConfigureDialog() const override;
+Q_SIGNALS:
+    void configChanged();
+};
