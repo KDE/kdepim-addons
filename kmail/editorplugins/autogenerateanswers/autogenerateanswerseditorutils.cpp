c45afda7f543c07656178ecb08978ddc44bc1e15
Laurent Montel <montel@kde.org>
2025-06-10 08:53:42 +0200

    Add autogenerateanswerseditorutils class

---
+++ kmail/editorplugins/autogenerateanswers/autogenerateanswerseditorutils.cpp
@@ -0,0 +1,10 @@
+/*
+   SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#include "autogenerateanswerseditorutils.h"
+
+AutoGenerateAnswersEditorUtils::AutoGenerateAnswersEditorUtils()
+{
+}
---
+++ kmail/editorplugins/autogenerateanswers/autogenerateanswerseditorutils.h
@@ -0,0 +1,12 @@
+/*
+   SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#pragma once
+
+class AutoGenerateAnswersEditorUtils
+{
+public:
+    AutoGenerateAnswersEditorUtils();
+};
