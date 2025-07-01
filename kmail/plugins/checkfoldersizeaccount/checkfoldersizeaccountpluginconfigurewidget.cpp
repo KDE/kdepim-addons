4892eec818b781275dddb8b91d80195cdeff0110
Laurent Montel <montel@kde.org>
2025-07-01 13:47:05 +0200

    Prepare to add limit

--- kmail/plugins/checkfoldersizeaccount/checkfoldersizeaccountpluginconfigurewidget.cpp
+++ kmail/plugins/checkfoldersizeaccount/checkfoldersizeaccountpluginconfigurewidget.cpp
@@ -5,17 +5,39 @@
 */
 
 #include "checkfoldersizeaccountpluginconfigurewidget.h"
+#include <KLocalizedString>
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QSpinBox>
 #include <QVBoxLayout>
 
 using namespace Qt::Literals::StringLiterals;
 CheckFolderSizeAccountPluginConfigureWidget::CheckFolderSizeAccountPluginConfigureWidget(QWidget *parent)
     : QWidget{parent}
+    , mWarningSpinBox(new QSpinBox(this))
 {
     auto mainLayout = new QVBoxLayout(this);
     mainLayout->setObjectName(u"mainLayout"_s);
     mainLayout->setContentsMargins({});
+    mainLayout->addWidget(new QLabel(i18n("Display Colored Folder when size is too expensive."), this));
+    auto hboxLayout = new QHBoxLayout;
+    hboxLayout->setContentsMargins({});
+    hboxLayout->setObjectName(u"hboxLayout"_s);
+    mainLayout->addLayout(hboxLayout);
+
+    // TODO add size
+    hboxLayout->addWidget(new QLabel(i18n("Size:"), this));
+    hboxLayout->addWidget(mWarningSpinBox);
 }
 
 CheckFolderSizeAccountPluginConfigureWidget::~CheckFolderSizeAccountPluginConfigureWidget() = default;
+
+void CheckFolderSizeAccountPluginConfigureWidget::load()
+{
+}
+
+void CheckFolderSizeAccountPluginConfigureWidget::save()
+{
+}
 
 #include "moc_checkfoldersizeaccountpluginconfigurewidget.cpp"
--- kmail/plugins/checkfoldersizeaccount/checkfoldersizeaccountpluginconfigurewidget.h
+++ kmail/plugins/checkfoldersizeaccount/checkfoldersizeaccountpluginconfigurewidget.h
@@ -6,11 +6,17 @@
 #pragma once
 
 #include <QWidget>
-
+class QSpinBox;
 class CheckFolderSizeAccountPluginConfigureWidget : public QWidget
 {
     Q_OBJECT
 public:
     explicit CheckFolderSizeAccountPluginConfigureWidget(QWidget *parent = nullptr);
     ~CheckFolderSizeAccountPluginConfigureWidget() override;
+
+    void load();
+    void save();
+
+private:
+    QSpinBox *const mWarningSpinBox;
 };
