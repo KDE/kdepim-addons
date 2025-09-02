4580f4d0ddd5c372f369a5b39154532afbf0569b
Laurent Montel <montel@kde.org>
2025-09-02 08:45:19 +0200

    Add close button

--- plugins/messageviewerplugins/common/messagevieweraiplugin/viewerpluginaiwidget.cpp
+++ plugins/messageviewerplugins/common/messagevieweraiplugin/viewerpluginaiwidget.cpp
@@ -5,12 +5,47 @@
 */
 
 #include "viewerpluginaiwidget.h"
+#include <KLocalizedString>
+#include <QStyle>
+#include <QToolButton>
 #include <QVBoxLayout>
+using namespace Qt::Literals::StringLiterals;
 ViewerPluginAIWidget::ViewerPluginAIWidget(QWidget *parent)
     : QWidget{parent}
+    , mCloseButton(new QToolButton(this))
 {
+    auto layout = new QVBoxLayout(this);
+    layout->setSpacing(0);
+    layout->setContentsMargins({});
+
+    auto hboxLayout = new QHBoxLayout;
+    hboxLayout->setSpacing(style()->pixelMetric(QStyle::PM_LayoutHorizontalSpacing));
+    hboxLayout->setContentsMargins(style()->pixelMetric(QStyle::PM_LayoutLeftMargin),
+                                   style()->pixelMetric(QStyle::PM_LayoutTopMargin),
+                                   style()->pixelMetric(QStyle::PM_LayoutRightMargin),
+                                   style()->pixelMetric(QStyle::PM_LayoutBottomMargin));
+    mCloseButton->setObjectName(u"close-button"_s);
+    mCloseButton->setIcon(QIcon::fromTheme(u"dialog-close"_s));
+    mCloseButton->setIconSize(QSize(16, 16));
+    mCloseButton->setToolTip(i18nc("@info:tooltip", "Close"));
+
+#ifndef QT_NO_ACCESSIBILITY
+    mCloseButton->setAccessibleName(i18n("Close"));
+#endif
+    mCloseButton->setAutoRaise(true);
+    hboxLayout->addWidget(mCloseButton);
+    connect(mCloseButton, &QToolButton::clicked, this, &ViewerPluginAIWidget::slotCloseWidget);
 }
 
 ViewerPluginAIWidget::~ViewerPluginAIWidget() = default;
+
+void ViewerPluginAIWidget::slotCloseWidget()
+{
+    if (isHidden()) {
+        return;
+    }
+    hide();
+    // TODO Q_EMIT toolsWasClosed();
+}
 
 #include "moc_viewerpluginaiwidget.cpp"
--- plugins/messageviewerplugins/common/messagevieweraiplugin/viewerpluginaiwidget.h
+++ plugins/messageviewerplugins/common/messagevieweraiplugin/viewerpluginaiwidget.h
@@ -7,11 +7,15 @@
 #pragma once
 
 #include <QWidget>
-
+class QToolButton;
 class ViewerPluginAIWidget : public QWidget
 {
     Q_OBJECT
 public:
     explicit ViewerPluginAIWidget(QWidget *parent = nullptr);
     ~ViewerPluginAIWidget() override;
+
+private:
+    void slotCloseWidget();
+    QToolButton *const mCloseButton;
 };
