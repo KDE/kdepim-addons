13eb38e76975fc5675ab376779247d5a9a19b756
Laurent Montel <montel@kde.org>
2025-09-11 13:39:29 +0200

    Prepare checkfoldersizeaccounttreeview

---
+++ kmail/plugins/checkfoldersizeaccount/checkfoldersizeaccounttreeview.cpp
@@ -0,0 +1,13 @@
+/*
+   SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#include "checkfoldersizeaccounttreeview.h"
+
+CheckFolderSizeAccountTreeView::CheckFolderSizeAccountTreeView(QWidget *parent)
+    : QTreeView(parent)
+{
+}
+
+CheckFolderSizeAccountTreeView::~CheckFolderSizeAccountTreeView() = default;
---
+++ kmail/plugins/checkfoldersizeaccount/checkfoldersizeaccounttreeview.h
@@ -0,0 +1,15 @@
+/*
+   SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#pragma once
+#include <QTreeView>
+
+class CheckFolderSizeAccountTreeView : public QTreeView
+{
+    Q_OBJECT
+public:
+    explicit CheckFolderSizeAccountTreeView(QWidget *parent = nullptr);
+    ~CheckFolderSizeAccountTreeView() override;
+};
