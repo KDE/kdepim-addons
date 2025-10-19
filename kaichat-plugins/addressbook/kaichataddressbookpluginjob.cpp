550607987cf7ce8e9f58315e5dc0ac2d63646ebe
Laurent Montel <montel@kde.org>
2025-10-19 14:45:04 +0200

    Add kaichataddressbookpluginjob

---
+++ kaichat-plugins/addressbook/kaichataddressbookpluginjob.cpp
@@ -0,0 +1,13 @@
+/*
+  SPDX-FileCopyrightText: 2025 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+#include "kaichataddressbookpluginjob.h"
+
+KAIChatAddressBookPluginJob::KAIChatAddressBookPluginJob(QObject *parent)
+    : QObject{parent}
+{
+}
+
+KAIChatAddressBookPluginJob::~KAIChatAddressBookPluginJob() = default;
