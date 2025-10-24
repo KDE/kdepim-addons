4ec9e3a51a0ba1ebf1608b22353b1820a21d8a74
Laurent Montel <montel@kde.org>
2025-10-24 13:30:51 +0200

    Continue to implement job

--- kaichat-plugins/addressbook/kaichataddressbookpluginjob.cpp
+++ kaichat-plugins/addressbook/kaichataddressbookpluginjob.cpp
@@ -5,7 +5,10 @@
 */
 #include "kaichataddressbookpluginjob.h"
 #include "kaichat_addressbookplugin_debug.h"
+#include "kaichataddressbookpluginutils.h"
+#include <KLocalizedString>
 
+using namespace Qt::Literals::StringLiterals;
 KAIChatAddressBookPluginJob::KAIChatAddressBookPluginJob(QObject *parent)
     : TextAutoGenerateText::TextAutoGenerateTextToolPluginJob{parent}
 {
@@ -22,11 +25,30 @@ void KAIChatAddressBookPluginJob::start()
     }
 
     Q_EMIT toolInProgress(i18n("Get Email..."));
+    QString result;
+    const QStringList lst = requiredArguments();
+    for (const auto &arg : lst) {
+        for (const auto &resultTool : std::as_const(mToolArguments)) {
+            if (resultTool.keyTool == arg) {
+                const QString value = resultTool.value;
+                if (arg == "addressbookinfo"_L1) {
+                    const KAIChatAddressBookPluginUtils::AddressBookEnum typeAddressBook = KAIChatAddressBookPluginUtils::convertStringToAddressBookEnum(value);
+                    switch (typeAddressBook) {
+                    case KAIChatAddressBookPluginUtils::AddressBookEnum::Email:
+                        // TODO result = i18n("Current time is %1", QLocale().toString(QTime::currentTime()));
+                        break;
+                    case KAIChatAddressBookPluginUtils::AddressBookEnum::Unknown:
+                        qCWarning(KAICHAT_ADDRESSBOOK_LOG) << "Invalid addressbook argument" << value;
+                        break;
+                    }
+                }
+            }
+        }
+    }
 
+    qDebug() << " toolArguments " << toolArguments();
+    Q_EMIT finished(result, mMessageUuid, mChatId, mToolIdentifier);
     deleteLater();
-    // TODO
-    Q_EMIT finished({}, {}, {}, {});
-    // TODO
 }
 
 #include "moc_kaichataddressbookpluginjob.cpp"
