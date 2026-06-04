01b00b93149d3dfef7c28bfc1c175ad07b23d859
Laurent Montel <montel@kde.org>
2026-06-04 08:16:11 +0200

    Fix clang-format

--- kmail/plugins/checkfoldersizeaccount/checkfoldersizeaccountcombobox.cpp
+++ kmail/plugins/checkfoldersizeaccount/checkfoldersizeaccountcombobox.cpp
@@ -17,9 +17,11 @@ CheckFolderSizeAccountComboBox::~CheckFolderSizeAccountComboBox() = default;
 void CheckFolderSizeAccountComboBox::fill()
 {
     addItem(i18nc("@label:combobox, All folders", "All"), CheckFolderSizeAccountFilterProxyModel::FolderSize::All);
-    addItem(i18nc("@label:combobox, Folders bigger than the specified size", "More Than 100 kB"), CheckFolderSizeAccountFilterProxyModel::FolderSize::MoreThan100K);
+    addItem(i18nc("@label:combobox, Folders bigger than the specified size", "More Than 100 kB"),
+            CheckFolderSizeAccountFilterProxyModel::FolderSize::MoreThan100K);
     addItem(i18nc("@label:combobox, Folders bigger than the specified size", "More Than 1 MB"), CheckFolderSizeAccountFilterProxyModel::FolderSize::MoreThan1M);
-    addItem(i18nc("@label:combobox, Folders bigger than the specified size", "More Than 10 MB"), CheckFolderSizeAccountFilterProxyModel::FolderSize::MoreThan10M);
+    addItem(i18nc("@label:combobox, Folders bigger than the specified size", "More Than 10 MB"),
+            CheckFolderSizeAccountFilterProxyModel::FolderSize::MoreThan10M);
 }
 
 CheckFolderSizeAccountFilterProxyModel::FolderSize CheckFolderSizeAccountComboBox::currentFolderSize() const
